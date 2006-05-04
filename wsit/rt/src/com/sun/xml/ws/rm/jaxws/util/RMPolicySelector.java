d1d4a2dee53eb500fbe383e264cab90762e75e15
Bhakti Mehta <bhaktimehta@java.net>
2006-05-04 21:41:12 +0000

    Added copyright information Issue number: Obtained from: Submitted by: Reviewed by:
    svn path=/trunk/; revision=47

--- wsit/rt/src/com/sun/xml/ws/rm/jaxws/util/RMPolicySelector.java
+++ wsit/rt/src/com/sun/xml/ws/rm/jaxws/util/RMPolicySelector.java
@@ -1,4 +1,26 @@
 /*
+  * The contents of this file are subject to the terms
+  * of the Common Development and Distribution License
+  * (the License).  You may not use this file except in
+  * compliance with the License.
+  *
+  * You can obtain a copy of the license at
+  * https://glassfish.dev.java.net/public/CDDLv1.0.html.
+  * See the License for the specific language governing
+  * permissions and limitations under the License.
+  *
+  * When distributing Covered Code, include this CDDL
+  * Header Notice in each file and include the License file
+  * at https://glassfish.dev.java.net/public/CDDLv1.0.html.
+  * If applicable, add the following below the CDDL Header,
+  * with the fields enclosed by brackets [] replaced by
+  * you own identifying information:
+  * "Portions Copyrighted [year] [name of copyright owner]"
+  *
+  * Copyright 2006 Sun Microsystems Inc. All Rights Reserved
+  */
+package com.sun.xml.ws.rm.jaxws.util;
+/*
  * RMPolicySelector.java
  *
  * @author Mike Grogan
@@ -6,15 +28,14 @@
  *
  */
 
-package com.sun.xml.ws.rm.jaxws.util;
+
+import com.sun.xml.ws.policy.PolicyAssertion;
+import com.sun.xml.ws.policy.spi.PolicySelector;
+import static com.sun.xml.ws.rm.RMConstants.*;
+
 import javax.xml.namespace.QName;
 import java.util.ArrayList;
-import com.sun.xml.ws.policy.PolicyAssertion;
-import com.sun.xml.ws.policy.spi.PolicySelector; 
 import java.util.Iterator;
-import static com.sun.xml.ws.rm.RMConstants.version;
-import static com.sun.xml.ws.rm.RMConstants.sunVersion;
-import static com.sun.xml.ws.rm.RMConstants.microsoftVersion;
 /**
  *
  */
