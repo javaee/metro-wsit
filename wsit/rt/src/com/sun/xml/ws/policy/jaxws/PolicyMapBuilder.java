a56265b900e02eeb5516507eb827aed5cd693e25
Fabian Ritzmann <ritzmann@java.net>
2006-05-04 11:55:19 +0000

    Simplified code.
    svn path=/trunk/; revision=31

--- wsit/rt/src/com/sun/xml/ws/policy/jaxws/PolicyMapBuilder.java
+++ wsit/rt/src/com/sun/xml/ws/policy/jaxws/PolicyMapBuilder.java
@@ -3,12 +3,12 @@
  * of the Common Development and Distribution License
  * (the License).  You may not use this file except in
  * compliance with the License.
- * 
+ *
  * You can obtain a copy of the license at
  * https://glassfish.dev.java.net/public/CDDLv1.0.html.
  * See the License for the specific language governing
  * permissions and limitations under the License.
- * 
+ *
  * When distributing Covered Code, include this CDDL
  * Header Notice in each file and include the License file
  * at https://glassfish.dev.java.net/public/CDDLv1.0.html.
@@ -16,7 +16,7 @@
  * with the fields enclosed by brackets [] replaced by
  * you own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- * 
+ *
  * Copyright 2006 Sun Microsystems Inc. All Rights Reserved
  */
 package com.sun.xml.ws.policy.jaxws;
@@ -34,62 +34,58 @@ import java.util.List;
  * Used for populating changes into PolicyMap. Once a PolicyMap is created
  * PolicyMapBuilder notifies all the registered WSPolicyBuilderHandler to populate
  * changes to the PolicyMap.
- * 
- * 
+ *
+ *
  * @author japod
  */
 class PolicyMapBuilder {
-    
-    
     /**
      * policyBuilders should contain list of registered PolicyBuilders
      */
-    private List<BuilderHandler> policyBuilders;
-    
-
+    private List<BuilderHandler> policyBuilders = new LinkedList<BuilderHandler>();
+        
     /**
      * Creates a new instance of PolicyMapBuilder
      */
     PolicyMapBuilder() {
     }
-    
-    
+        
     /**
      *     Registers another builder, which has to be notified after a new
-     *     PolicyMap is created in order to populate it's changes. 
-     *     
+     *     PolicyMap is created in order to populate it's changes.
+     *
      */
     void registerHandler(BuilderHandler builder){
-        if (null!=builder) {
-            getPolicyBuilders().add(builder);
+        if (null != builder) {
+            policyBuilders.add(builder);
         }
     }
-
+    
     /**
-     * Iterates all the registered PolicyBuilders and lets them populate 
+     * Iterates all the registered PolicyBuilders and lets them populate
      * their changes into PolicyMap
      */
     PolicyMap getPolicyMap() throws PolicyException{
         return getNewPolicyMap(null);
-    }    
-
+    }
+    
     /**
-     * Iterates all the registered PolicyBuilders and lets them populate 
+     * Iterates all the registered PolicyBuilders and lets them populate
      * their changes into PolicyMap. Registers mutator given as a parameter
      * with the newly created map.
      */
     PolicyMap getPolicyMap(PolicyMapMutator mutator) throws PolicyException{
-        if (null==mutator) {
+        if (null == mutator) {
             return getNewPolicyMap(null);
         } else {
             LinkedList<PolicyMapMutator> list = new LinkedList<PolicyMapMutator>();
             list.add(mutator);
             return getNewPolicyMap(list);
         }
-    }    
+    }
     
     /**
-     * Iterates all the registered PolicyBuilders and lets them populate 
+     * Iterates all the registered PolicyBuilders and lets them populate
      * their changes into PolicyMap. Registers mutators from collection given as a parameter
      * with the newly created map.
      */
@@ -99,7 +95,7 @@ class PolicyMapBuilder {
     
     
     /**
-     * Iterates all the registered PolicyBuilders and lets them populate 
+     * Iterates all the registered PolicyBuilders and lets them populate
      * their changes into PolicyMap. Registers mutators from collection given as a parameter
      * with the newly created map.
      */
@@ -107,27 +103,18 @@ class PolicyMapBuilder {
         HashSet<PolicyMapMutator> mutators = new HashSet<PolicyMapMutator>();
         PolicyMapExtender myExtender = PolicyMapExtender.createPolicyMapExtender();
         mutators.add(myExtender);
-        if(null!=externalMutators) {
+        if (null != externalMutators) {
             mutators.addAll(externalMutators);
         }
         PolicyMap policyMap = PolicyMap.createPolicyMap(mutators);
-        for(BuilderHandler builder : getPolicyBuilders()){
+        for(BuilderHandler builder : policyBuilders){
             builder.populate(myExtender);
         }
         return policyMap;
     }
     
-    /**
-     * private policyBuilder getter method
-     */
-    private List<BuilderHandler> getPolicyBuilders(){
-        if(null==this.policyBuilders)
-            this.policyBuilders = new LinkedList<BuilderHandler>();
-        return this.policyBuilders;
-    }
-    
     void unregisterAll() {
         this.policyBuilders = null;
     }
-
+    
 }
