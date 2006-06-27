b9f6df48a6dc3deb63a294ba681f5cf8e03a4b30
ofung <ofung@localhost>
2006-06-27 21:58:36 +0000

    Copyright update for 2006 Issue number: Obtained from: Submitted by: Reviewed by:
    svn path=/trunk/; revision=207

--- wsit/rt/src/com/sun/xml/ws/security/policy/Issuer.java
+++ wsit/rt/src/com/sun/xml/ws/security/policy/Issuer.java
@@ -1,5 +1,5 @@
 /*
- * $Id: Issuer.java,v 1.2 2006-06-15 09:50:23 venu Exp $
+ * $Id: Issuer.java,v 1.3 2006-06-27 21:58:31 ofung Exp $
  */
 
 /*
@@ -7,12 +7,12 @@
  * of the Common Development and Distribution License
  * (the License).  You may not use this file except in
  * compliance with the License.
- *
+ * 
  * You can obtain a copy of the license at
  * https://glassfish.dev.java.net/public/CDDLv1.0.html.
  * See the License for the specific language governing
  * permissions and limitations under the License.
- *
+ * 
  * When distributing Covered Code, include this CDDL
  * Header Notice in each file and include the License file
  * at https://glassfish.dev.java.net/public/CDDLv1.0.html.
@@ -20,7 +20,7 @@
  * with the fields enclosed by brackets [] replaced by
  * you own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- *
+ * 
  * Copyright 2006 Sun Microsystems Inc. All Rights Reserved
  */
 
--- wsit/rt/src/com/sun/xml/wss/impl/misc/DefaultCallbackHandler.java
+++ wsit/rt/src/com/sun/xml/wss/impl/misc/DefaultCallbackHandler.java
@@ -1,5 +1,5 @@
 /*
- * $Id: DefaultCallbackHandler.java,v 1.2 2006-06-14 11:45:42 kumarjayanti Exp $
+ * $Id: DefaultCallbackHandler.java,v 1.3 2006-06-27 21:58:33 ofung Exp $
  *
  */
 /*
@@ -7,12 +7,12 @@
  * of the Common Development and Distribution License
  * (the License).  You may not use this file except in
  * compliance with the License.
- *
+ * 
  * You can obtain a copy of the license at
  * https://glassfish.dev.java.net/public/CDDLv1.0.html.
  * See the License for the specific language governing
  * permissions and limitations under the License.
- *
+ * 
  * When distributing Covered Code, include this CDDL
  * Header Notice in each file and include the License file
  * at https://glassfish.dev.java.net/public/CDDLv1.0.html.
@@ -20,7 +20,7 @@
  * with the fields enclosed by brackets [] replaced by
  * you own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- *
+ * 
  * Copyright 2006 Sun Microsystems Inc. All Rights Reserved
  */
 
