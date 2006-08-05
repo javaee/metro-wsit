c896139bb43961e056e7aca97c90826b74b2d9d5
Arun Gupta <arungupta@java.net>
2006-08-05 00:29:37 +0000

    updated the copyright
    svn path=/trunk/; revision=375

--- wsit/samples/pricequote/src/pricequote/util/STSImpl.java
+++ wsit/samples/pricequote/src/pricequote/util/STSImpl.java
@@ -1,17 +1,27 @@
 /*
- * STSImpl.java
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License).  You may not use this file except in
+ * compliance with the License.
  *
- * Created on March 29, 2006, 12:33 AM
+ * You can obtain a copy of the license at
+ * https://glassfish.dev.java.net/public/CDDLv1.0.html.
+ * See the License for the specific language governing
+ * permissions and limitations under the License.
  *
- * To change this template, choose Tools | Options and locate the template under
- * the Source Creation and Management node. Right-click the template and choose
- * Open. You can then make changes to the template in the Source Editor.
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at https://glassfish.dev.java.net/public/CDDLv1.0.html.
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * you own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * Copyright 2006 Sun Microsystems Inc. All Rights Reserved
  */
-
-/**
- *
- * @author localuser
- */
+/*
+ $Id: STSImpl.java,v 1.2 2006-08-05 00:29:36 arungupta Exp $
+*/
 
 package pricequote.util;
 
