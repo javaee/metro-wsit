d1dd8a7ea8a71acd433cae6ba159cbe3d4762648
Harsha R. Aswath <raharsha@java.net>
2006-08-09 10:58:27 +0000

    added remaining  secure conversation tests
    svn path=/trunk/; revision=396

---
+++ wsit/test/e2e/testcases/wssc/sc1/server/PingImpl.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2005 Sun Microsystems, Inc.
+ * All rights reserved.
+ */
+package wssc.sc1.server;
+import javax.xml.ws.Holder;
+
+
+@javax.jws.WebService (endpointInterface="wssc.sc1.server.IPingService")
+@javax.xml.ws.BindingType(value="http://java.sun.com/xml/ns/jaxws/2003/05/soap/bindings/HTTP/")
+public class PingImpl implements IPingService {
+    
+   public void ping( Holder<String> scenario,
+         Holder<String> origin,
+         Holder<String> text){
+        System.out.println("The message is here : " + scenario.value + " " +origin.value + " " + text.value);
+
+    }                    
+}
