11ee62e21c2fb314cd4e7f6836b8f23aab2b45c2
mayankmishra <mayankmishra@localhost>
2006-08-25 12:56:07 +0000

    XWSS unit test file
    svn path=/trunk/; revision=482

--- wsit/rt/test/unit/src/com/sun/xml/wss/impl/SignAllHeadersTest.java
+++ wsit/rt/test/unit/src/com/sun/xml/wss/impl/SignAllHeadersTest.java
@@ -1,4 +1,25 @@
 /*
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License).  You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the license at
+ * https://glassfish.dev.java.net/public/CDDLv1.0.html.
+ * See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at https://glassfish.dev.java.net/public/CDDLv1.0.html.
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * you own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * Copyright 2006 Sun Microsystems Inc. All Rights Reserved
+ */
+/*
  * SignAllHeadersTest.java
  *
  * Created on March 31, 2006, 11:36 AM
@@ -108,7 +129,7 @@ public class SignAllHeadersTest extends TestCase{
             
             WSSAssertion wssAssertion = null;
             AssertionSet as = null;
-            Policy wssPolicy = new PolicyResourceLoader().loadPolicy("policy-binding2.xml");
+            Policy wssPolicy = new PolicyResourceLoader().loadPolicy("security/policy-binding2.xml");
             Iterator<AssertionSet> i = wssPolicy.iterator();
             if(i.hasNext())
                 as = i.next();
--- wsit/rt/test/unit/src/com/sun/xml/wss/impl/SymmetricBindingTest.java
+++ wsit/rt/test/unit/src/com/sun/xml/wss/impl/SymmetricBindingTest.java
@@ -1,3 +1,24 @@
+/*
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License).  You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the license at
+ * https://glassfish.dev.java.net/public/CDDLv1.0.html.
+ * See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at https://glassfish.dev.java.net/public/CDDLv1.0.html.
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * you own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * Copyright 2006 Sun Microsystems Inc. All Rights Reserved
+ */
 package com.sun.xml.wss.impl;
 
 import java.util.*;
@@ -110,7 +131,7 @@ public class SymmetricBindingTest extends TestCase{
         
             WSSAssertion wssAssertion = null;
             AssertionSet as = null;
-            Policy wssPolicy = new PolicyResourceLoader().loadPolicy("policy-binding2.xml");
+            Policy wssPolicy = new PolicyResourceLoader().loadPolicy("security/policy-binding2.xml");
             Iterator<AssertionSet> i = wssPolicy.iterator();
             if(i.hasNext())
                 as = i.next();
@@ -183,7 +204,7 @@ public class SymmetricBindingTest extends TestCase{
        
        WSSAssertion wssAssertion = null;
        AssertionSet as = null;
-       Policy wssPolicy = new PolicyResourceLoader().loadPolicy("./etc/policy-binding2.xml");
+       Policy wssPolicy = new PolicyResourceLoader().loadPolicy("security/policy-binding2.xml");
        Iterator<AssertionSet> i = wssPolicy.iterator();
        if(i.hasNext())
            as = i.next();
