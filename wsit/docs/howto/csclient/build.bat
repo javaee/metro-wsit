f433ee21e513e282aeb9120b20201994f8f7a611
Rebecca Searls <rebeccas@java.net>
2006-09-05 21:19:50 +0000

    added MS client code
    svn path=/trunk/; revision=567

---
+++ wsit/docs/howto/csclient/build.bat
@@ -0,0 +1,2 @@
+svcutil /config:Client.exe.config http://localhost:8080/wsit-enabled-fromjava/addnumbers?wsdl
+C:\WINNT\Microsoft.NET\Framework\v2.0.50727\csc.exe /r:"C:\WINNT\Microsoft.NET\Framework\v3.0\Windows Communication Foundation\System.ServiceModel.dll" /r:"C:\WINNT\Microsoft.NET\Framework\v3.0\Windows Communication Foundation\System.Runtime.Serialization.dll" Client.cs AddNumbersImplService.cs
