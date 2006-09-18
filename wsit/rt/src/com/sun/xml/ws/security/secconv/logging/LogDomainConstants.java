b4f705a72d0fb467a8cf0cff0b340c9d25abd6a7
manveen <manveen@localhost>
2006-09-18 23:00:55 +0000

    Adding logging related setup for defining domains and adding the properties file for logging.
    svn path=/trunk/; revision=628

---
+++ wsit/rt/src/com/sun/xml/ws/security/secconv/logging/LogDomainConstants.java
@@ -0,0 +1,49 @@
+/*
+ * $Id: LogDomainConstants.java,v 1.1 2006-09-18 23:00:54 manveen Exp $
+ */
+
+/*
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License).  You may not use this file except in
+ * compliance with the License.
+ * 
+ * You can obtain a copy of the license at
+ * https://glassfish.dev.java.net/public/CDDLv1.0.html.
+ * See the License for the specific language governing
+ * permissions and limitations under the License.
+ * 
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at https://glassfish.dev.java.net/public/CDDLv1.0.html.
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * you own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ * 
+ * Copyright 2006 Sun Microsystems Inc. All Rights Reserved
+ */
+
+package com.sun.xml.ws.security.secconv.logging;
+
+//import com.sun.xml.wss.*;
+
+/**
+ * @author Manveen Kaur
+ *
+ * This interface defines a number of constants pertaining to Logging domains.
+ */
+
+public interface LogDomainConstants {
+    
+    public static final String MODULE_TOP_LEVEL_DOMAIN =
+            "javax.enterprise.resource.xml.webservices.security.secconv";
+    
+    public static final String WSSC_IMPL_DOMAIN = MODULE_TOP_LEVEL_DOMAIN;
+    
+    public static final String PACKAGE_ROOT = "com.sun.xml.ws.security.secconv.logging";
+    
+    public static final String WSSC_IMPL_DOMAIN_BUNDLE =
+            PACKAGE_ROOT + ".LogStrings";
+        
+}
