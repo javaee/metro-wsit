3fa51a879953957d63576d27c1078f7c2a7ea1cd
hofsass <hofsass@localhost>
2006-10-05 23:53:27 +0000

    Fixing WSTrust & XWSS tests... still a problem with WSTrust all other tests pass.
    svn path=/trunk/; revision=771

---
+++ wsit/test/e2e/testcases/xwss/s4/server/PingImpl.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2005 Sun Microsystems, Inc.
+ * All rights reserved.
+ */
+package xwss.s4.server;
+
+
+@javax.jws.WebService (endpointInterface="xwss.s4.server.IPingService")
+@javax.xml.ws.BindingType(value="http://java.sun.com/xml/ns/jaxws/2003/05/soap/bindings/HTTP/")
+public class PingImpl implements IPingService {
+    
+   public String ping(String message) {
+        System.out.println("The message is here : " + message);
+        return message;
+    }                    
+}
