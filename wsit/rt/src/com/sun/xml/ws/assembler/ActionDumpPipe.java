369bd0341a7cb21c4bae0e5e9fe0f2ab4205ac5c
Arun Gupta <arungupta@java.net>
2006-10-05 00:09:21 +0000

    fixed the compilation failure
    svn path=/trunk/; revision=737

--- wsit/rt/src/com/sun/xml/ws/assembler/ActionDumpPipe.java
+++ wsit/rt/src/com/sun/xml/ws/assembler/ActionDumpPipe.java
@@ -18,7 +18,7 @@
  [name of copyright owner]
 */
 /*
- $Id: ActionDumpPipe.java,v 1.1 2006-09-22 00:51:16 arungupta Exp $
+ $Id: ActionDumpPipe.java,v 1.2 2006-10-05 00:09:21 arungupta Exp $
 
  Copyright (c) 2006 Sun Microsystems, Inc.
  All rights reserved.
@@ -26,16 +26,9 @@
 
 package com.sun.xml.ws.assembler;
 
-import javax.xml.namespace.QName;
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamReader;
-import javax.xml.ws.addressing.AddressingBuilderFactory;
-import javax.xml.ws.addressing.AddressingConstants;
-
-import com.sun.xml.ws.api.message.Header;
+import com.sun.xml.ws.api.WSBinding;
 import com.sun.xml.ws.api.message.Message;
 import com.sun.xml.ws.api.message.Packet;
-import com.sun.xml.ws.api.model.wsdl.WSDLPort;
 import com.sun.xml.ws.api.pipe.Pipe;
 import com.sun.xml.ws.api.pipe.PipeCloner;
 import com.sun.xml.ws.api.pipe.helper.AbstractFilterPipeImpl;
@@ -45,17 +38,16 @@ import com.sun.xml.ws.api.pipe.helper.AbstractFilterPipeImpl;
  */
 public class ActionDumpPipe extends AbstractFilterPipeImpl {
     private final String name;
+    private final WSBinding binding;
 
-    private WSDLPort wsdlPort;
-
-    public ActionDumpPipe(WSDLPort wsdlPort, Pipe next) {
-        this("ActionDumpPipe", wsdlPort, next);
+    public ActionDumpPipe(WSBinding binding, Pipe next) {
+        this("ActionDumpPipe", binding, next);
     }
 
-    public ActionDumpPipe(String name, WSDLPort wsdlPort, Pipe next) {
+    public ActionDumpPipe(String name, WSBinding binding, Pipe next) {
         super(next);
         this.name = name;
-        this.wsdlPort = wsdlPort;
+        this.binding = binding;
     }
 
     /**
@@ -64,7 +56,7 @@ public class ActionDumpPipe extends AbstractFilterPipeImpl {
     private ActionDumpPipe(ActionDumpPipe that, PipeCloner cloner) {
         super(that, cloner);
         this.name = that.name;
-        this.wsdlPort = that.wsdlPort;
+        this.binding = that.binding;
     }
 
     public Packet process(Packet packet) {
@@ -83,8 +75,8 @@ public class ActionDumpPipe extends AbstractFilterPipeImpl {
         try {
             Message m = packet.getMessage().copy();
 
-            String to = getHeaderValue(m, TO_QNAME);
-            String action = getHeaderValue(m, ACTION_QNAME);
+            String to = m.getHeaders().getTo(binding.getAddressingVersion(), binding.getSOAPVersion());
+            String action = m.getHeaders().getAction(binding.getAddressingVersion(), binding.getSOAPVersion());
 
             System.out.println("{To, Action}: {" + to + ", " + action + "}");
         } catch (Exception e) {
@@ -92,22 +84,7 @@ public class ActionDumpPipe extends AbstractFilterPipeImpl {
         }
     }
 
-    protected static String getHeaderValue(Message m, QName headerName) throws XMLStreamException {
-        Header h = m.getHeaders().get(headerName, false);
-
-        if (h == null)
-            return null;
-
-        XMLStreamReader xsr = h.readHeader();
-        xsr.next();
-        return xsr.getText();
-    }
-
     public Pipe copy(PipeCloner cloner) {
         return new ActionDumpPipe(this, cloner);
     }
-
-    private static final AddressingConstants ac = AddressingBuilderFactory.newInstance().newAddressingBuilder().newAddressingConstants();
-    private static final QName ACTION_QNAME = ac.getActionQName();
-    private static final QName TO_QNAME = ac.getToQName();
 }
