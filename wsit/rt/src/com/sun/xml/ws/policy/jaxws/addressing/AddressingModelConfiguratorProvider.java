b337aa08fcb6d69903617bc8f4a58be8a24c8b88
Jakub Podlesak <jakub.podlesak@oracle.com>
2006-10-06 13:08:13 +0000

    Temporarily hardcoded addressing assertions qnames and added some logging
    svn path=/trunk/; revision=780

--- wsit/rt/src/com/sun/xml/ws/policy/jaxws/addressing/AddressingModelConfiguratorProvider.java
+++ wsit/rt/src/com/sun/xml/ws/policy/jaxws/addressing/AddressingModelConfiguratorProvider.java
@@ -32,10 +32,11 @@ import com.sun.xml.ws.policy.PolicyAssertion;
 import com.sun.xml.ws.policy.PolicyException;
 import com.sun.xml.ws.policy.PolicyMap;
 import com.sun.xml.ws.policy.PolicyMapKey;
-import com.sun.xml.ws.policy.jaxws.WSDLPolicyMapWrapper;
 import com.sun.xml.ws.policy.jaxws.spi.ModelConfiguratorProvider;
+import com.sun.xml.ws.policy.privateutil.PolicyLogger;
 import java.util.Iterator;
 import javax.xml.namespace.QName;
+import javax.xml.ws.WebServiceFeature;
 
 /**
  *
@@ -43,9 +44,14 @@ import javax.xml.namespace.QName;
  */
 public class AddressingModelConfiguratorProvider implements ModelConfiguratorProvider{
     
+    private static final PolicyLogger logger = PolicyLogger.getLogger(AddressingModelConfiguratorProvider.class);
+    
     private static final QName[] AddressingAssertions = {
-        new QName(AddressingVersion.MEMBER.nsUri,"UsingAddressing"),
-        new QName(AddressingVersion.W3C.nsUri,"UsingAddressing")};
+        new QName("http://www.w3.org/2006/05/addressing/wsdl","UsingAddressing"),
+        new QName("http://schemas.xmlsoap.org/ws/2004/09/policy/addressing","UsingAddressing")};
+    // TODO: replace hardcoded uris with the constants bellow
+    // new QName(AddressingVersion.MEMBER.nsUri,"UsingAddressing"),
+    // new QName(AddressingVersion.W3C.nsUri,"UsingAddressing")};
     
     /**
      * Creates a new instance of AddressingModelConfiguratorProvider
@@ -61,29 +67,49 @@ public class AddressingModelConfiguratorProvider implements ModelConfiguratorPro
      * @param policyMap must be non-null
      */
     public void configure(WSDLModel model, PolicyMap policyMap) throws PolicyException {
+        logger.entering("configure", new Object[]{model, policyMap});
         if ((null==model) || (null==policyMap)) {
             return;
         }
         for (WSDLService service:model.getServices().values()) {
+            // TODO: clean up logging
+            logger.fine("configure", "processing service " + service.getName().toString());
             for (WSDLPort port : service.getPorts()) {
                 PolicyMapKey key = PolicyMap.createWsdlEndpointScopeKey(service.getName(),port.getName());
                 Policy policy = policyMap.getEndpointEffectivePolicy(key);
+                // TODO: clean up logging
+                logger.fine("configure", "processing port " + port.getName().toString());
                 for (QName addressingAssertionQName : AddressingAssertions) {
+                    // TODO: clean up logging
+                    logger.fine("configure", "processing assertion " + addressingAssertionQName.toString());
                     if (null!=policy && policy.contains(addressingAssertionQName)) {
+                        // TODO: clean up logging
+                        logger.fine("configure", "assertion contained in the policy");
                         Iterator <AssertionSet> assertions = policy.iterator();
                         while(assertions.hasNext()){
                             AssertionSet assertionSet = assertions.next();
                             Iterator<PolicyAssertion> policyAssertion = assertionSet.iterator();
                             while(policyAssertion.hasNext()){
                                 PolicyAssertion assertion = policyAssertion.next();
+                                // TODO: clean up logging
+                                logger.fine("configure", "assertion = " + assertion.getName());
                                 if(assertion.getName().equals(addressingAssertionQName) && !assertion.isOptional()){
-                                    port.addFeature(AddressingVersion.getFeature(addressingAssertionQName.getNamespaceURI(), true, true));
+                                    WebServiceFeature feature = AddressingVersion.getFeature(addressingAssertionQName.getNamespaceURI(), true, true);
+                                    // TODO: clean up logging
+                                    logger.fine("configure", "calling port.addFeature " + feature);
+                                    port.addFeature(feature);
+                                    // TODO: clean up logging
+                                    logger.fine("configure", "feature added");
                                 } // end-if non optional wsa assertion found
                             } // next assertion
                         } // next alternative
+                    }else {
+                        // TODO: clean up logging
+                        logger.fine("configure", "assertion " + addressingAssertionQName.toString() + " not contained in the policy");
                     } // end-if policy contains wsa assertion
                 } // end foreach port
             } //end foreach addr assertion
         } // end foreach service
+        logger.exiting("configure");
     }
 }
