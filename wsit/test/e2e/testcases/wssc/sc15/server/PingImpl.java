ae043389f7f90866607aaeab54c1c88f76246e92
manveen <manveen@localhost>
2006-10-18 19:29:53 +0000

    Adding wssc scenario 15 to e2e test harness.
    svn path=/trunk/; revision=947

---
+++ wsit/test/e2e/testcases/wssc/sc15/server/PingImpl.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2006 Sun Microsystems, Inc.
+ * All rights reserved.
+ */
+package wssc.sc15.server;
+import javax.xml.ws.Holder;
+
+
+@javax.jws.WebService (endpointInterface="wssc.sc15.server.IPingService")
+@javax.xml.ws.BindingType(value="http://java.sun.com/xml/ns/jaxws/2003/05/soap/bindings/HTTP/")
+public class PingImpl implements IPingService {
+    
+   public void ping( Holder<String> scenario,
+         Holder<String> origin,
+         Holder<String> text){
+        System.out.println("The message is here : " + scenario.value + " " +origin.value + " " + text.value);
+
+    }                    
+}
