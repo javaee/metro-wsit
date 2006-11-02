3a0095c374592a863d418d20eeb9e3527be4a33e
Oleksiy Stashok <oleksiy.stashok@oracle.com>
2006-11-02 16:45:59 +0000

    Move SOAP/TCP under rt
    svn path=/trunk/; revision=1133

---
+++ wsit/rt/src/com/sun/xml/ws/transport/tcp/servicechannel/stubs/OpenChannelResponse.java
@@ -0,0 +1,84 @@
+/*
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License).  You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the license at
+ * https://glassfish.dev.java.net/public/CDDLv1.0.html.
+ * See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at https://glassfish.dev.java.net/public/CDDLv1.0.html.
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * you own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * Copyright 2006 Sun Microsystems Inc. All Rights Reserved
+ */
+
+package com.sun.xml.ws.transport.tcp.servicechannel.stubs;
+
+import com.sun.xml.ws.transport.tcp.util.ChannelSettings;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for openChannelResponse complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="openChannelResponse">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="return" type="{http://servicechannel.tcp.transport.ws.xml.sun.com/}channelSettings" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "openChannelResponse", propOrder = {
+    "_return"
+})
+public class OpenChannelResponse {
+
+    @XmlElement(name = "return")
+    protected ChannelSettings _return;
+
+    /**
+     * Gets the value of the return property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link ChannelSettings }
+     *     
+     */
+    public ChannelSettings getReturn() {
+        return _return;
+    }
+
+    /**
+     * Sets the value of the return property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link ChannelSettings }
+     *     
+     */
+    public void setReturn(ChannelSettings value) {
+        this._return = value;
+    }
+
+}
