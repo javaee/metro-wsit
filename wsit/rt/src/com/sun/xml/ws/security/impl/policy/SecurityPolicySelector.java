1bac216d75cd1d86472df2dddbacc21b9312449d
Ashutosh Shahi <venu@java.net>
2006-11-13 08:54:54 +0000

    Enable creating of DisableStreamingSecurity assertion
    svn path=/trunk/; revision=1226

--- wsit/rt/src/com/sun/xml/ws/security/impl/policy/SecurityPolicySelector.java
+++ wsit/rt/src/com/sun/xml/ws/security/impl/policy/SecurityPolicySelector.java
@@ -3,12 +3,12 @@
  * of the Common Development and Distribution License
  * (the License).  You may not use this file except in
  * compliance with the License.
- * 
+ *
  * You can obtain a copy of the license at
  * https://glassfish.dev.java.net/public/CDDLv1.0.html.
  * See the License for the specific language governing
  * permissions and limitations under the License.
- * 
+ *
  * When distributing Covered Code, include this CDDL
  * Header Notice in each file and include the License file
  * at https://glassfish.dev.java.net/public/CDDLv1.0.html.
@@ -16,7 +16,7 @@
  * with the fields enclosed by brackets [] replaced by
  * you own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- * 
+ *
  * Copyright 2006 Sun Microsystems Inc. All Rights Reserved
  */
 package com.sun.xml.ws.security.impl.policy;
@@ -115,7 +115,7 @@ public class SecurityPolicySelector implements PolicySelector{
         supportedAssertions.add(new QName(SECURITY_POLICY_NS,WssRelV10Token11));
         supportedAssertions.add(new QName(SECURITY_POLICY_NS,WssRelV20Token11));
         //     supportedAssertions.add(new QName(SECURITY_POLICY_NS,X509V3Token));
-
+        
         supportedAssertions.add(new QName(SECURITY_POLICY_NS,SignedSupportingTokens));
         supportedAssertions.add(new QName(SECURITY_POLICY_NS,EndorsingSupportingTokens));
         
@@ -158,6 +158,9 @@ public class SecurityPolicySelector implements PolicySelector{
         supportedAssertions.add(new QName(TRUST_NS,SignWith));
         
         supportedAssertions.add(new QName(TRUST_NS,EncryptWith));
+        supportedAssertions.add(new QName("http://schemas.sun.com/2006/03/wss/server","DisableStreamingSecurity"));
+        supportedAssertions.add(new QName("http://schemas.sun.com/2006/03/wss/client","DisableStreamingSecurity"));
+        
         
         //supportedAssertions.add(new QName(SUN_WSS_SECURITY_CLIENT_POLICY_NS,CallbackHandler));
         //supportedAssertions.add(new QName(SUN_WSS_SECURITY_CLIENT_POLICY_NS,KeyStore));
