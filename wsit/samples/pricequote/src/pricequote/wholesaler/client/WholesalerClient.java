0335b0b912b21ead9d6e6f32c31a69866d946c0b
Arun Gupta <arungupta@java.net>
2006-12-13 23:32:40 +0000

    cleaned up previously commented code
    svn path=/trunk/; revision=1419

--- wsit/samples/pricequote/src/pricequote/wholesaler/client/WholesalerClient.java
+++ wsit/samples/pricequote/src/pricequote/wholesaler/client/WholesalerClient.java
@@ -20,23 +20,17 @@
  * Copyright 2006 Sun Microsystems Inc. All Rights Reserved
  */
 /*
- $Id: WholesalerClient.java,v 1.4 2006-12-07 00:04:38 jdg6688 Exp $
+ $Id: WholesalerClient.java,v 1.5 2006-12-13 23:32:40 arungupta Exp $
 */
 
 package pricequote.wholesaler.client;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.annotation.Resource;
 import javax.xml.namespace.QName;
-import javax.xml.ws.BindingProvider;
 import javax.xml.ws.WebServiceContext;
 import javax.xml.ws.WebServiceException;
-
-import com.sun.xml.ws.security.trust.WSTrustConstants;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * @author Arun Gupta
@@ -46,12 +40,6 @@ public class WholesalerClient {
     WebServiceContext wsc;
 
     WholesalerPortType port = null;
-    private static final String STS_ENDPOINT = "sts.Endpoint";
-    private static final String STS_WSDL_LOCATION = "sts.WsdlLocation";
-    private static final String STS_NAMESPACE = "namespace";
-    private static final String STS_SERVICE_NAME = "sts.ServiceName";
-    private static final String STS_PORT_NAME = "sts.PortName";
-
     public static void main(String[] args) {
         String endpoint = "http://localhost:8080/pricequote/wholesaler?wsdl";
         String serviceName = "WholesalerQuoteService";
@@ -77,50 +65,11 @@ public class WholesalerClient {
         try {
             WholesalerQuoteService service = new WholesalerQuoteService(new URL(endpoint), name);
             port = service.getWholesalerPort();
-           // configureSTS((BindingProvider)port);
         } catch (MalformedURLException e) {
             throw new WebServiceException(e);
         }
     }
 
- /*   private static final void configureSTS(BindingProvider port) {
-        Map<String,String> map = configureMicrosoftSTS();
-
-        try {
-            port.getRequestContext().put(WSTrustConstants.PROPERTY_SERVICE_END_POINT, new URL(map.get(STS_ENDPOINT)));
-            port.getRequestContext().put(WSTrustConstants.PROPERTY_URL, new URL(map.get(STS_WSDL_LOCATION)));
-            port.getRequestContext().put(WSTrustConstants.PROPERTY_SERVICE_NAME, new QName(map.get(STS_NAMESPACE), map.get(STS_SERVICE_NAME)));
-            port.getRequestContext().put(WSTrustConstants.PROPERTY_PORT_NAME, new QName(map.get(STS_NAMESPACE), map.get(STS_PORT_NAME)));
-        } catch (MalformedURLException e) {
-            throw new WebServiceException(e);
-        }
-    }
-
-    private static final Map<String,String> configureMicrosoftSTS() {
-        Map<String,String> map = new HashMap<String,String>();
-
-        map.put(STS_ENDPOINT, "http://localhost:8080/pricequote-wcf/sts");
-        map.put(STS_WSDL_LOCATION, "http://localhost:8080/pricequote-wcf/sts?wsdl");
-        map.put(STS_NAMESPACE, "http://tempuri.org/");
-        map.put(STS_SERVICE_NAME, "SecurityTokenService");
-        map.put(STS_PORT_NAME, "WSHttpBinding_ISecurityTokenService");
-
-        return map;
-    }
-
-    private static final Map<String,String> configureSunSTS() {
-        Map<String,String> map = new HashMap<String,String>();
-
-        map.put(STS_ENDPOINT, "http://localhost:8080/pricequote/sts");
-        map.put(STS_WSDL_LOCATION, "http://localhost:8080/pricequote/sts?wsdl");
-        map.put(STS_NAMESPACE, "http://tempuri.org/");
-        map.put(STS_SERVICE_NAME, "SecurityTokenServiceSun");
-        map.put(STS_PORT_NAME, "CustomBinding_ISecurityTokenService");
-
-        return map;
-    }
-*/
-    
     public Quote getQuote(int pid) {
         return port.getQuote(pid);
     }
