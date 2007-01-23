bfe55b3c61e138ce211f6377b089e9ea2abbb304
Ashutosh Shahi <venu@java.net>
2007-01-23 11:20:16 +0000

    Fix for bug 314
    svn path=/trunk/; revision=1584
    
    
    Former-commit-id: 7464d61d514557f9d45ecb332c00322abb6a6e81

--- wsit/rt/src/com/sun/xml/ws/addressing/impl/policy/Address.java
+++ wsit/rt/src/com/sun/xml/ws/addressing/impl/policy/Address.java
@@ -3,12 +3,12 @@
  * of the Common Development and Distribution License
  * (the License).  You may not use this file except in
  * compliance with the License.
- * 
+ *
  * You can obtain a copy of the license at
  * https://glassfish.dev.java.net/public/CDDLv1.0.html.
  * See the License for the specific language governing
  * permissions and limitations under the License.
- * 
+ *
  * When distributing Covered Code, include this CDDL
  * Header Notice in each file and include the License file
  * at https://glassfish.dev.java.net/public/CDDLv1.0.html.
@@ -16,7 +16,7 @@
  * with the fields enclosed by brackets [] replaced by
  * you own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- * 
+ *
  * Copyright 2006 Sun Microsystems Inc. All Rights Reserved
  */
 
@@ -60,7 +60,9 @@ public class Address extends com.sun.xml.ws.policy.PolicyAssertion implements co
     private void populate() {
         if ( !populated ) {
             try {
-                this.address = new URI(this.getValue());
+                if(this.getValue() != null){
+                    this.address = new URI(this.getValue().trim());
+                }
                 populated = true;
             } catch (URISyntaxException ex) {
                 if(logger.getLevel() == Level.SEVERE){
@@ -75,7 +77,7 @@ public class Address extends com.sun.xml.ws.policy.PolicyAssertion implements co
         return address;
     }
     
-       
+    
     public String getNamespaceURI() {
         throw new UnsupportedOperationException();
     }
