d8feda88bd2ae77c2d5c5f3a3ca9bd0e5f294519
Harsha R. Aswath <raharsha@java.net>
2007-01-25 06:39:52 +0000

    clean up of imports
    svn path=/trunk/; revision=1607
    
    
    Former-commit-id: c5ea6dec53c69364e2cc3fc82e1f965543ba5122

--- wsit/test/e2e/testcases/wstrust/scenario5/sts/STSImpl.java
+++ wsit/test/e2e/testcases/wstrust/scenario5/sts/STSImpl.java
@@ -7,30 +7,8 @@
 
 package wstrust.scenario5.sts;
 
-import com.sun.xml.ws.policy.Policy;
-import com.sun.xml.ws.policy.PolicyAssertion;
-import com.sun.xml.ws.policy.PolicyException;
-import com.sun.xml.ws.policy.sourcemodel.PolicyModelTranslator;
-import com.sun.xml.ws.policy.sourcemodel.PolicyModelUnmarshaller;
-import com.sun.xml.ws.policy.sourcemodel.PolicySourceModel;
-import com.sun.xml.ws.security.IssuedTokenContext;
-import com.sun.xml.ws.security.impl.IssuedTokenContextImpl;
-import com.sun.xml.ws.security.trust.WSTrustElementFactory;
-import com.sun.xml.ws.security.trust.WSTrustContract;
-import com.sun.xml.ws.security.trust.WSTrustFactory;
-import com.sun.xml.ws.security.trust.elements.RequestSecurityToken;
-import com.sun.xml.ws.security.trust.elements.RequestSecurityTokenResponse;
-import com.sun.xml.ws.security.trust.impl.STSConfiguration;
-import com.sun.xml.ws.security.trust.impl.TrustSPMetadata;
 import com.sun.xml.ws.security.trust.sts.BaseSTSImpl;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
 import javax.annotation.Resource;
-import javax.xml.namespace.QName;
 
 import javax.xml.ws.Provider;
 import javax.xml.ws.Service;
