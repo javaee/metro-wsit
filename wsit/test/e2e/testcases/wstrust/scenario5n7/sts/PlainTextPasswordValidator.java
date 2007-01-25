e04e9609dd415524f5261b9c1ab8915d211063f4
Harsha R. Aswath <raharsha@java.net>
2007-01-25 09:55:17 +0000

    added new test for code coverage improvement
    svn path=/trunk/; revision=1611
    
    
    Former-commit-id: 1b2893ab4c51c04838029a435cbc5f49e34a3562

---
+++ wsit/test/e2e/testcases/wstrust/scenario5n7/sts/PlainTextPasswordValidator.java
@@ -0,0 +1,21 @@
+// package common;
+package wstrust.scenario5n7.sts;
+
+import com.sun.xml.wss.impl.callback.PasswordValidationCallback;
+
+
+public class PlainTextPasswordValidator implements PasswordValidationCallback.PasswordValidator {
+                                                                                                        
+                                                                                                        
+        public boolean validate(PasswordValidationCallback.Request request)
+            throws PasswordValidationCallback.PasswordValidationException {
+            System.out.println("Using configured PlainTextPasswordValidator................");                                                                                                        
+            PasswordValidationCallback.PlainTextPasswordRequest plainTextRequest =
+                (PasswordValidationCallback.PlainTextPasswordRequest) request;
+            if ("Alice".equals(plainTextRequest.getUsername()) &&
+                "abcd!1234".equals(plainTextRequest.getPassword())) {
+                return true;
+            }
+            return false;
+        }
+}
