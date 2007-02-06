d6a0459f5aad83c4a75ac599db667c30cd9ef9e9
hofsass <hofsass@localhost>
2007-02-06 19:31:39 +0000

    Fix for WSIT issue 349
    svn path=/trunk/; revision=1666
    
    
    Former-commit-id: 230a52ec4f99c83b0bf68a05172f33e5fd717b8c

--- wsit/docs/howto/wsit-enabled-fromjava/src/fromjava/client/AddNumbersClient.java
+++ wsit/docs/howto/wsit-enabled-fromjava/src/fromjava/client/AddNumbersClient.java
@@ -41,12 +41,14 @@
  */
 package fromjava.client;
 
+import com.sun.xml.ws.Closeable;
 import java.rmi.RemoteException;
 
 public class AddNumbersClient {
     public static void main (String[] args) {
+        AddNumbersImpl port = null;
         try {
-            AddNumbersImpl port = new AddNumbersImplService().getAddNumbersImplPort();
+            port = new AddNumbersImplService().getAddNumbersImplPort();
             
             int number1 = 10;
             int number2 = 20;
@@ -61,6 +63,8 @@ public class AddNumbersClient {
             System.out.printf ("The result of adding %d and %d is %d.\n", number1, number2, result);
         } catch (AddNumbersException_Exception ex) {
             System.out.printf ("Caught AddNumbersException_Exception: %s\n", ex.getFaultInfo ().getDetail ());
-        }
+        } finally {
+            ((Closeable)port).close();
+        }        
     }
 }
