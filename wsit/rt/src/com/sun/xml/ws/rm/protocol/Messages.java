23ce326bebf270e45994d3f7f441384bec7e24d5
Bhakti Mehta <bhaktimehta@java.net>
2007-08-31 20:38:49 +0000

    Committing work for the two RM versions support in a separate branch for now Issue number: Obtained from: Submitted by: Reviewed by:
    svn path=/branches/wsrm1_1-branch/; revision=2527
    
    
    Former-commit-id: 8de62f46df869534398afa5adf20a2fb3a080e65

---
+++ wsit/rt/src/com/sun/xml/ws/rm/protocol/Messages.java
@@ -0,0 +1,67 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * 
+ * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * 
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License. You can obtain
+ * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
+ * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ * 
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
+ * Sun designates this particular file as subject to the "Classpath" exception
+ * as provided by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code.  If applicable, add the following below the License
+ * Header, with the fields enclosed by brackets [] replaced by your own
+ * identifying information: "Portions Copyrighted [year]
+ * [name of copyright owner]"
+ * 
+ * Contributor(s):
+ * 
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+
+package com.sun.xml.ws.rm.protocol;
+
+import java.util.ResourceBundle;
+import java.text.MessageFormat;
+
+/**
+ * @author Bhakti Mehta
+ * @author Mike Grogan
+ */
+public enum Messages {
+
+    ACKREQUESTED_TOSTRING_STRING, //2 args
+    ACKNOWLEDGEMENT_MESSAGE, //2 args
+    BOTH_ACKS_AND_NACKS_MESSAGE, //0 args
+    UPPERBOUND_LESSTHAN_LOWERBOUND_MESSAGE, //0 args
+    SEQUENCE_ACKNOWLEDGEMENT_TOSTRING_STRING, //2 args
+    SEQUENCE_TOSTRING_STRING //3 args
+
+    ;
+
+    private static final ResourceBundle rb = ResourceBundle.getBundle(com.sun.xml.ws.rm.protocol.Messages.class.getName());
+
+    public String toString() {
+        return format();
+    }
+
+     /** Loads a string resource and formats it with specified arguments. */
+    public String format( Object... args ) {
+        return MessageFormat.format( com.sun.xml.ws.rm.protocol.Messages.rb.getString(name()), args );
+    }
+}
