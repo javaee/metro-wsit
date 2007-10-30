f19e9ac2f60bedb2a17e765503287333261ac882
Marek Potociar <marek.potociar@oracle.com>
2007-10-30 18:00:48 +0000

    Code reformatted + 1 TODO added
    svn path=/trunk/; revision=2726
    
    
    Former-commit-id: 73a338d7c41994a97517ec432ac3811352ac6843

--- wsit/rt/src/com/sun/xml/ws/rm/Sequence.java
+++ wsit/rt/src/com/sun/xml/ws/rm/Sequence.java
@@ -1,5 +1,5 @@
 /*
- * $Id: Sequence.java,v 1.12 2007-09-20 18:47:07 bhaktimehta Exp $
+ * $Id: Sequence.java,v 1.13 2007-10-30 18:00:48 m_potociar Exp $
  */
 
 /*
@@ -37,8 +37,6 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-
-
 package com.sun.xml.ws.rm;
 
 import com.sun.xml.ws.rm.jaxws.runtime.SequenceConfig;
@@ -49,24 +47,20 @@ import java.util.ArrayList;
  *  A Sequence is a sparse array of messages corresponding to an RM Sequence.  It
  *  is implemented as an ArrayList with nulls at unfilled indices.
  */
-
 public class Sequence {
-   
+
     /**
      * The sequence identifier.
      */
     protected String id;
-    
     /**
      * The underlying list of messages
      */
     protected ArrayList<Message> list;
-    
     /**
      * The smallest unfilled index.
      */
     protected int nextIndex = 1;
-    
     /**
      * Flag indicates that message with Sequence header containing
      * Last element has been sent/received.  If this is the case,
@@ -75,46 +69,38 @@ public class Sequence {
      * message in the sequence.
      */
     protected boolean last = false;
-
     /**
      * Flag that indicates if the CloseSequence message has been sent/received
      * If this is the case then the Sequence needs to be closed and no more messages
      * with that Sequence Id should be accepted
      */
     protected boolean closed = false;
-    
     /**
      * Maximum number of stored messages.  Used for server-side sequences
      * for which flow control is enabled.  The value -1 indicates that
      * there is no limit.
      */
     protected int maxMessages = -1;
-    
-    
     /**
      * Number of messages currently being stored awaiting completion.
      */
     protected int storedMessages = 0;
-    
-    
     /**
      * Last accesse time.
      */
     protected long lastActivityTime;
-    
     protected boolean allowDuplicates;
-
     /**
      * RMConstants associated with each Sequence which
      * will give information regarding addressing version, JAXBContext etc
      *
      */
-    protected  RMConstants rmConstants;
+    protected RMConstants rmConstants;
 
+    // TODO: replace protected access with getter + constructor setter
     protected SequenceConfig config;
-    
-   
     protected int firstKnownGap;
+
     /**
      * Gets the sequence identifier
      * @return The sequence identifier.
@@ -122,7 +108,7 @@ public class Sequence {
     public String getId() {
         return id;
     }
-    
+
     /**
      * Sets the sequence identifier.
      * @param id The sequence identifier.
@@ -130,9 +116,9 @@ public class Sequence {
     public void setId(String id) {
         this.id = id;
     }
-    
+
     public Sequence() {
-        
+
         list = new ArrayList<Message>();
         //fill in 0-th index that will never be used since
         //messageNumbers are 1-based and we will be keeping
@@ -141,17 +127,17 @@ public class Sequence {
         allowDuplicates = false;
         firstKnownGap = 1;
 
-	resetLastActivityTime();
+        resetLastActivityTime();
 
     }
-    
+
     /**
      * Accessor for SequenceConfig field.
      */
     public SequenceConfig getSequenceConfig() {
         return config;
     }
-    
+
     /**
      * Accessor for the nextIndex field.
      * @return The value of the nextIndex field
@@ -159,7 +145,7 @@ public class Sequence {
     public synchronized int getNextIndex() {
         return nextIndex;
     }
-    
+
     /**
      * Gets the Message at the specified index.
      * @param index The index to access
@@ -168,17 +154,15 @@ public class Sequence {
      * index used.
      */
     public synchronized Message get(int index)
-                throws InvalidMessageNumberException {
-        
+            throws InvalidMessageNumberException {
+
         if (index >= nextIndex) {
             throw new InvalidMessageNumberException(
                     Messages.INVALID_INDEX_MESSAGE.format(index));
         }
         return list.get(index);
     }
-    
-    
-    
+
     /**
      * Adds a Message to the Sequence at a specified indes.  The index must be
      * positive.  If the index is larger than the nextIndex field, nulls are inserted 
@@ -189,51 +173,49 @@ public class Sequence {
      *
      * @return The new value of nextIndex.
      */
-    public synchronized int set(int i, Message m) 
-            throws InvalidMessageNumberException, 
-                    BufferFullException,
-                    DuplicateMessageException {
-       
+    public synchronized int set(int i, Message m)
+            throws InvalidMessageNumberException,
+            BufferFullException,
+            DuplicateMessageException {
+
         //record the index and sequence in the message
         m.setMessageNumber(i);
         m.setSequence(this);
-        
+
         if (i <= 0) {
             throw new InvalidMessageNumberException();
         }
-        
+
         if (storedMessages == maxMessages) {
             throw new BufferFullException(this);
         }
-        
+
         if (i < nextIndex) {
             Message mess = null;
-            if (null != (mess = list.get(i))  && !allowDuplicates) {
+            if (null != (mess = list.get(i)) && !allowDuplicates) {
                 //Store the original message in the exception so
                 //that exception handling can use it.
                 throw new DuplicateMessageException(mess);
             }
-            
+
             list.set(i, m);
         } else if (i == nextIndex) {
-            list.add( m);
+            list.add(m);
             nextIndex++;
         } else {
             //fill in nulls between nextIndex an new nextIndex.
             for (int j = nextIndex; j < i; j++) {
                 list.add(null);
             }
-            list.add( m);
+            list.add(m);
             nextIndex = i + 1;
         }
-       
+
         storedMessages++;
-        
+
         return i;
     }
-    
-   
-    
+
     /**
      * Sets the last flag.
      */
@@ -247,7 +229,7 @@ public class Sequence {
     public synchronized void setClosed() {
         closed = true;
     }
-    
+
     /*
      * Gets the value of the last flag.
      *
@@ -265,23 +247,20 @@ public class Sequence {
     public synchronized boolean isClosed() {
         return closed;
     }
-     
+
     /////////////////////////////////////////////////////////////////////////////
     /*
      *             InactivityTimeout management helpers
      *             used by ClientOutboundSequence and ServerInboundSequence
      *
      *////////////////////////////////////////////////////////////////////////////
-    
-    
     /**
      * Resets lastActivityTime field to current time.
      */
     public void resetLastActivityTime() {
         lastActivityTime = System.currentTimeMillis();
     }
- 
-    
+
     /**
      * Accessor for lastActivityTime field.
      *
@@ -290,7 +269,7 @@ public class Sequence {
     protected long getLastActivityTime() {
         return lastActivityTime;
     }
-    
+
     /**
      * Return value determines whether elapsed time is close enough to
      * time limit to pull the trigger and send an ackRequested to keep the
@@ -301,8 +280,6 @@ public class Sequence {
      */
     protected boolean isGettingClose(long elapsedTime, long timeLimit) {
         //for now
-        return elapsedTime > timeLimit / 2; 
+        return elapsedTime > timeLimit / 2;
     }
-    
-  
 }
