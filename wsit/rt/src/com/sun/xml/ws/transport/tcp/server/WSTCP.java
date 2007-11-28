aac9275388c8c4620fbbc82632b193b57de99ad6
Oleksiy Stashok <oleksiy.stashok@oracle.com>
2007-11-28 16:01:37 +0000

    make possible for SOAP/TCP standalone mode to listen on several tcp ports, depending on webservice(s) configuration in wsdl
    svn path=/trunk/; revision=2856
    
    
    Former-commit-id: 47ce51760a3a3527bf8d86787dcd602408f4a040

--- wsit/rt/src/com/sun/xml/ws/transport/tcp/server/WSTCP.java
+++ wsit/rt/src/com/sun/xml/ws/transport/tcp/server/WSTCP.java
@@ -41,10 +41,16 @@ import com.sun.xml.ws.transport.http.DeploymentDescriptorParser;
 import com.sun.xml.ws.transport.tcp.resources.MessagesMessages;
 import com.sun.xml.ws.transport.tcp.util.WSTCPURI;
 import com.sun.xml.ws.transport.tcp.grizzly.GrizzlyTCPConnector;
+import com.sun.xml.ws.transport.tcp.util.TCPConstants;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URL;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -57,11 +63,14 @@ public final class WSTCP {
     
     private static final String JAXWS_RI_RUNTIME = "WEB-INF/sun-jaxws.xml";
     
+    private static final String ENABLE_PROTOCOL_CHECK = "-enableProtocolCheck";
+
     private final TCPContext context;
     private final ClassLoader initClassLoader;
     private WSTCPDelegate delegate;
-    private WSTCPConnector connector;
+    private Collection<WSTCPConnector> connectors;
     private final String contextPath;
+    private boolean isProtocolCheck;
     
     public WSTCP(@NotNull final TCPContext context,
             @NotNull final ClassLoader initClassLoader,
@@ -69,6 +78,14 @@ public final class WSTCP {
         this.context = context;
         this.initClassLoader = initClassLoader;
         this.contextPath = contextPath;
+    }
+
+    public boolean isProtocolCheck() {
+        return isProtocolCheck;
+    }
+
+    public void setProtocolCheck(boolean isProtocolCheck) {
+        this.isProtocolCheck = isProtocolCheck;
     }
     
     public @NotNull List<TCPAdapter> parseDeploymentDescriptor() throws IOException {
@@ -79,50 +96,76 @@ public final class WSTCP {
         return parser.parse(sunJaxWsXml.toExternalForm(), sunJaxWsXml.openStream());
     }
     
-    public @NotNull WSTCPConnector initialize() throws IOException {
+    public @NotNull Collection<WSTCPConnector> initialize() throws IOException {
         final List<TCPAdapter> adapters = parseDeploymentDescriptor();
         delegate = new WSTCPDelegate();
+        Collection<WSTCPConnector> connectors = new LinkedList<WSTCPConnector>();
+        
+        Iterator<TCPAdapter> it = adapters.iterator();
+        while(it.hasNext()) {
+            TCPAdapter adapter = it.next();
+            final URI uri = adapter.getEndpoint().getPort().getAddress().getURI();
+            final WSTCPURI tcpURI = WSTCPURI.parse(uri);
+
+            if (isProtocolCheck && 
+                    !TCPConstants.PROTOCOL_SCHEMA.equals(uri.getScheme())) {
+                logger.log(Level.INFO, 
+                        MessagesMessages.STANDALONE_ADAPTER_NOT_REGISTERED(
+                        adapter.name, adapter.urlPattern));
+                it.remove();
+                continue;
+            }
+            
+            final WSTCPConnector connector = new GrizzlyTCPConnector(tcpURI.host,
+                    tcpURI.port, delegate);
+            connector.listen();
+            connectors.add(connector);
+            logger.log(Level.FINE,
+                    MessagesMessages.STANDALONE_ADAPTER_REGISTERED(
+                    adapter.name, adapter.urlPattern));
+        }
+    
         delegate.registerAdapters(contextPath, adapters);
-        
-        final TCPAdapter adapter = adapters.get(0);
-        final URI uri = adapter.getEndpoint().getPort().getAddress().getURI();
-        
-        final WSTCPURI tcpURI = WSTCPURI.parse(uri);
-        
-        final WSTCPConnector connector = new GrizzlyTCPConnector(tcpURI.host,
-                tcpURI.port, delegate);
-        connector.listen();
-        return connector;
+        return connectors;
     }
     
     public void process() throws IOException {
-        connector = initialize();
+        connectors = initialize();
     }
     
     public void close() {
-        if (connector != null) {
+        if (connectors != null) {
+            for(WSTCPConnector connector : connectors)
             connector.close();
         }
     }
     
     public static void main(final String[] args) {
-        if (args.length != 1) {
+        Set<String> params = new HashSet<String>();
+        
+        if (args.length < 1) {
             System.out.println(MessagesMessages.STANDALONE_RUN());
             System.exit(0);
         }
         
-        final String contextPath = args[0];
+        for(int i=1; i<args.length; i++) {
+            params.add(args[i]);
+        }
         
+        final String contextPath = args[0];
+
         final ClassLoader classloader = Thread.currentThread().getContextClassLoader();
         final TCPContext context = new TCPStandaloneContext(classloader);
         
         final WSTCP wsTCP = new WSTCP(context, classloader, contextPath);
+        wsTCP.setProtocolCheck(params.contains(ENABLE_PROTOCOL_CHECK));
         
         try {
             wsTCP.process();
             System.out.println(MessagesMessages.STANDALONE_EXIT());
             System.in.read();
         } catch (Exception e) {
+            logger.log(Level.SEVERE, MessagesMessages.STANDALONE_EXCEPTION(), e);
         } finally {
             wsTCP.close();
         }
