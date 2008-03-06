0e6b3a7cf3bf5f9bebdb740d9c050f5741d73357
Fabian Ritzmann <ritzmann@java.net>
2008-03-06 15:10:50 +0000

    Issue 831: Fine-tuned logging and added Javadoc.
    svn path=/trunk/; revision=3207
    
    
    Former-commit-id: 23914a0a5d838836e4b23b7099973d74d3126dbd

--- wsit/rt/src/com/sun/xml/ws/mex/server/MEXAddressResolver.java
+++ wsit/rt/src/com/sun/xml/ws/mex/server/MEXAddressResolver.java
@@ -35,15 +35,20 @@
  */
 package com.sun.xml.ws.mex.server;
 
-import com.sun.istack.NotNull;
-import com.sun.xml.ws.api.server.PortAddressResolver;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.logging.Logger;
 import javax.xml.namespace.QName;
 
+import com.sun.istack.NotNull;
+import com.sun.xml.ws.api.server.PortAddressResolver;
+import com.sun.xml.ws.mex.MessagesMessages;
+
 /**
- *
+ * Implementation of the JAX-WS callback interface.
+ * 
+ * Returns an address that JAX-WS uses to replace the current WSDL port address.
+ * 
  * @author Fabian Ritzmann
  */
 class MEXAddressResolver extends PortAddressResolver {
@@ -53,6 +58,13 @@ class MEXAddressResolver extends PortAddressResolver {
     private final String port;
     private final String address;
 
+    /**
+     * Initialize the class.
+     * 
+     * @param serviceName The name of the underlying service.
+     * @param portName The name of the underlying port.
+     * @param address The location address that MEX computed for this port.
+     */
     MEXAddressResolver(@NotNull final QName serviceName, @NotNull final QName portName,
             @NotNull final String address) {
         this.service = serviceName;
@@ -65,6 +77,23 @@ class MEXAddressResolver extends PortAddressResolver {
         return this.address;
     }
 
+    /**
+     * Return a new address that JAX-WS uses to overwrite the current WSDL port
+     * address. This method implements the following algorithm:
+     * 
+     * <ol>
+     * <li>Only return a new address if the WSDL port and service names match.
+     * (Returns null otherwise, which means that JAX-WS does not change the existing address.)
+     * <li>If the original address has an "http" protocol prefix and the new address
+     * has "https", return the original address.
+     * <li>Otherwise return the new address.
+     * </ol>
+     * 
+     * @param serviceName The WSDL service name. May not be null.
+     * @param portName The WSDL port name. May not be null.
+     * @param currentAddress The current location address in the WSDL.
+     * @return A new location address.
+     */
     @Override
     public String getAddressFor(@NotNull final QName serviceName, @NotNull final String portName,
             final String currentAddress) {
@@ -81,12 +110,16 @@ class MEXAddressResolver extends PortAddressResolver {
                         if (currentAddressUrl.getProtocol().toLowerCase().equals("http") &&
                                 addressUrl.getProtocol().toLowerCase().equals("https")) {
                             result = currentAddress;
+                            LOGGER.fine(MessagesMessages.MEX_0019_LEAVE_ADDRESS(currentAddress, portName));
+                        }
+                        else {
+                            LOGGER.fine(MessagesMessages.MEX_0018_REPLACE_ADDRESS(currentAddress, portName, result));
                         }
                     } catch (MalformedURLException ex) {
-                        Logger.getLogger(MEXAddressResolver.class.getName()).fine(ex.toString());
+                        LOGGER.fine(MessagesMessages.MEX_0020_CURRENT_ADDRESS_NO_URL(currentAddress, portName));
                     }
                 } catch (MalformedURLException ex) {
-                    Logger.getLogger(MEXAddressResolver.class.getName()).fine(ex.toString());
+                    LOGGER.fine(MessagesMessages.MEX_0021_NEW_ADDRESS_NO_URL(this.address, portName));
                 }
             }
         }
