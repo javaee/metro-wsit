6ad834132eecaea1d843484eb9e491ef60b65a6d
Marek Potociar <marek.potociar@oracle.com>
2008-05-02 21:49:30 +0000

    UC2 build script
    svn path=/trunk/; revision=3513
    
    
    Former-commit-id: 0b54f584157cb08b5b1e48892da2d0cc3f566f36

---
+++ wsit/uc2.py
@@ -0,0 +1,12 @@
+pkg = {
+    "name"          : "metro",
+    "version"       : "1.2,0-",
+    "attributes"    : { 
+        "description"      : "Metro Web Services Stack for GlassFish",
+        "description_long" : "Metro is a high-performance, extensible, \
+easy-to-use web service stack. It is a one-stop shop for all your web service \
+needs, from the simplest hello world web service to reliable, secured, and \
+transacted web service that involves .NET services."
+      },
+    "dirtrees"      : [ "glassfish" ],
+}
