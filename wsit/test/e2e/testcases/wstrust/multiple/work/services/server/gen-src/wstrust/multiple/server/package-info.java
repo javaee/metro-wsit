0859e4829f373e2987933af15c05f5ad656c1fdc
jdg6688 <jdg6688@localhost>
2008-10-06 02:51:13 +0000

    delete
    svn path=/trunk/; revision=4163
    
    
    Former-commit-id: 4f6d6fb3cf1cdc9c219991167671a39580238a0f

--- wsit/test/e2e/testcases/wstrust/multiple/work/services/server/gen-src/wstrust/multiple/server/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://xmlsoap.org/Ping", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package wstrust.multiple.server;
--- wsit/test/e2e/testcases/wstrust/multiple/work/services/sts/gen-src/wstrust/multiple/sts/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://schemas.microsoft.com/Message")
-package wstrust.multiple.sts;
