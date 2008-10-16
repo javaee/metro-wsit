ae0d961d6c2c9eed359add5309f01f00ee6ef610
Marek Potociar <marek.potociar@oracle.com>
2008-10-16 14:39:06 +0000

    New version of UC2 Metro module descriptor
    svn path=/trunk/; revision=4203
    
    
    Former-commit-id: ae195a1ba9eadf071961aae8c28c40abbcfeefe9

--- wsit/uc2.py
+++ wsit/uc2.py
@@ -2,13 +2,14 @@ pkg = {
     "name"          : "metro",
     "version"       : "",
     "attributes"    : { 
-        "description"      : "Metro Web Services Stack for GlassFish",
-        "description_long" : "Metro is a high-performance, extensible, \
+        "pkg.summary"         :    "Metro Web Services Stack for GlassFish",
+        "pkg.description"     :    "Metro is a high-performance, extensible, \
 easy-to-use web service stack. It is a one-stop shop for all your web service \
 needs, from the simplest hello world web service to reliable, secured, and \
-transacted web service that involves .NET services."
+transacted web service that involves .NET services.",
+        "info.classification" : "Web Services"
       },
     "depends"       : { "pkg:/glassfish-web" : {"type" : "require" } },
     "dirtrees"      : [ "glassfish" ],
-    "licenses"      : { "glassfish/modules/Metro-LICENSE.txt"    : {"license" : "Metro-LICENSE.txt" } },
+    "licenses"      : { "glassfish/modules/Metro-LICENSE.txt"    : {"license" : "CDDL+GPL" } },
 }
