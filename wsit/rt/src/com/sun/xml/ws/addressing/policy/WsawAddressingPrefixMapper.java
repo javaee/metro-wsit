1be12aa2f07d28711141433118c1372530a8d75e
Fabian Ritzmann <ritzmann@java.net>
2009-01-21 13:08:17 +0000

    Removed unused import that prevented compilation.
    svn path=/branches/jaxws22-policy-migration/; revision=4597
    
    
    Former-commit-id: e45081f2bc7f00c0faf4aec28fc8d7c705666b42

--- wsit/rt/src/com/sun/xml/ws/addressing/policy/WsawAddressingPrefixMapper.java
+++ wsit/rt/src/com/sun/xml/ws/addressing/policy/WsawAddressingPrefixMapper.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  * 
- * Copyright 1997-2008 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2009 Sun Microsystems, Inc. All rights reserved.
  * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -37,7 +37,6 @@ package com.sun.xml.ws.addressing.policy;
 
 import com.sun.xml.ws.api.addressing.AddressingVersion;
 import com.sun.xml.ws.policy.spi.PrefixMapper;
-import com.sun.xml.ws.addressing.W3CAddressingMetadataConstants;
 
 import java.util.HashMap;
 import java.util.Map;
