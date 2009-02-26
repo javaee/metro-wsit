a23f64696fa3762bd35707a4183f8abf7c3239ae
Fabian Ritzmann <ritzmann@java.net>
2009-02-26 15:28:09 +0000

    PolicyExtensionHandler was moved to JAX-WS under a different package. Fix NetBeans warnings.
    svn path=/trunk/; revision=4717
    
    
    Former-commit-id: 479fac82abc96c93a2599f8638dcd3d4d4b843e9

--- wsit/tools/test/unit/src/com/sun/tools/ws/policy/jaxws/PolicyExtensionHandlerTest.java
+++ wsit/tools/test/unit/src/com/sun/tools/ws/policy/jaxws/PolicyExtensionHandlerTest.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  * 
- * Copyright 1997-2008 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2009 Sun Microsystems, Inc. All rights reserved.
  * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -97,9 +97,11 @@ public class PolicyExtensionHandlerTest extends TestCase {
         final Listener listener = new Listener();
         // another fake impl
         ErrorReceiverFilter receiver = new ErrorReceiverFilter(listener) {
+            @Override
             public void info(SAXParseException exception) {
             }
             
+            @Override
             public void warning(SAXParseException exception) {
             }
             
