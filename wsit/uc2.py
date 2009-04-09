f1383663b25138c726f8954597859b05288de945
Marek Potociar <marek.potociar@oracle.com>
2009-04-09 13:55:41 +0000

    Updated scripts publishing Metro GFv3 module to staging UC2
    svn path=/branches/METRO-1_4-BRANCH/; revision=4847
    
    
    Former-commit-id: b1c8df828ff389a5f3bf478d3b55b6009c567f9b

--- wsit/uc2.py
+++ wsit/uc2.py
@@ -1,14 +1,15 @@
 pkg = {
     "name"          : "metro",
-    "version"       : "",
+    "version"       : "@{version}",
     "attributes"    : { 
-        "description"      : "Metro Web Services Stack for GlassFish",
-        "description_long" : "Metro is a high-performance, extensible, \
+        "pkg.summary"      : "Metro Web Services Stack for GlassFish",
+        "pkg.description" : "Metro is a high-performance, extensible, \
 easy-to-use web service stack. It is a one-stop shop for all your web service \
 needs, from the simplest hello world web service to reliable, secured, and \
-transacted web service that involves .NET services."
+transacted web service that involves .NET services.",
+        "info.classification" : "Web Services"
       },
-    "depends"       : { "pkg:/glassfish-web" : {"type" : "require" } },
     "dirtrees"      : [ "glassfish" ],
+    "depends"       : { "pkg:/glassfish-web" : {"type" : "require" } },
     "licenses"      : { "glassfish/modules/Metro-LICENSE.txt"    : {"license" : "Metro-LICENSE.txt" } },
 }
