ef58acc99fc741098948e110d655a63a1b97d97f
Fabian Ritzmann <ritzmann@java.net>
2009-06-25 12:33:03 +0000

    Added more tests.
    svn path=/trunk/; revision=5057
    
    
    Former-commit-id: a7c0a686e2744595c9db45af087186b60f384f31

--- wsit/rt/test/unit/src/com/sun/xml/ws/management/server/ManagementWSDLPatcherTest.java
+++ wsit/rt/test/unit/src/com/sun/xml/ws/management/server/ManagementWSDLPatcherTest.java
@@ -98,6 +98,58 @@ public class ManagementWSDLPatcherTest extends TestCase {
   + "</service>"
   + "</definitions>";
 
+    private static final String WSDL = "<?xml version='1.0' encoding='UTF-8'?>"
+  + "<definitions xmlns:wsu=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd\" xmlns:wsp=\"http://www.w3.org/ns/ws-policy\" xmlns:wsp1_2=\"http://schemas.xmlsoap.org/ws/2004/09/policy\" xmlns:wsam=\"http://www.w3.org/2007/05/addressing/metadata\" xmlns:soap=\"http://schemas.xmlsoap.org/wsdl/soap/\" xmlns:tns=\"http://test.ws.xml.sun.com/\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns=\"http://schemas.xmlsoap.org/wsdl/\" targetNamespace=\"http://test.ws.xml.sun.com/\" name=\"NewWebServiceService\">"
+  + "<wsp:Policy wsu:Id=\"output-policy\">"
+  + "<All/>"
+  + "</wsp:Policy>"
+  + "<types>"
+  + "<xsd:schema>"
+  + "<xsd:import namespace=\"http://test.ws.xml.sun.com/\" schemaLocation=\"http://localhost:8080/WebApplicationSunJAXWSFromJava/NewWebService?xsd=1\" />"
+  + "</xsd:schema>"
+  + "</types>"
+  + "<message name=\"echo\">"
+  + "<part name=\"parameters\" element=\"tns:echo\" />"
+  + "</message>"
+  + "<message name=\"echoResponse\">"
+  + "<part name=\"parameters\" element=\"tns:echoResponse\" />"
+  + "</message>"
+  + "<portType name=\"NewWebService\">"
+  + "<operation name=\"echo\">"
+  + "<input wsam:Action=\"http://test.ws.xml.sun.com/NewWebService/echoRequest\" message=\"tns:echo\" />"
+  + "<output wsam:Action=\"http://test.ws.xml.sun.com/NewWebService/echoResponse\" message=\"tns:echoResponse\" />"
+  + "</operation>"
+  + "</portType>"
+  + "<binding name=\"NewWebServicePortBinding\" type=\"tns:NewWebService\">"
+  + "<wsp:PolicyReference URI=\"#binding-policy\"/>"
+  + "<soap:binding transport=\"http://schemas.xmlsoap.org/soap/http\" style=\"document\" />"
+  + "<operation name=\"echo\">"
+  + "<soap:operation soapAction=\"\" />"
+  + "<input>"
+  + "<wsp:Policy wsu:Id=\"inlined-policy\">"
+  + "<All/>"
+  + "</wsp:Policy>"
+  + "<soap:body use=\"literal\" />"
+  + "</input>"
+  + "<output>"
+  + "<wsp:PolicyReference URI=\"#output-policy\"/>"
+  + "<soap:body use=\"literal\" />"
+  + "</output>"
+  + "</operation>"
+  + "</binding>"
+  + "<service name=\"NewWebServiceService\">"
+  + "<port name=\"NewWebServicePort\" binding=\"tns:NewWebServicePortBinding\">"
+  + "<soap:address location=\"http://localhost:8080/WebApplicationSunJAXWSFromJava/NewWebService\" />"
+  + "</port>"
+  + "</service>"
+  + "<wsp:Policy wsu:Id=\"binding-policy\">"
+  + "<All/>"
+  + "</wsp:Policy>"
+  + "<wsp:Policy wsu:Id=\"unreferenced-policy\">"
+  + "<All/>"
+  + "</wsp:Policy>"
+  + "</definitions>";
+
     private DocumentBuilder builder;
 
 
@@ -159,6 +211,51 @@ public class ManagementWSDLPatcherTest extends TestCase {
         assertEquals(1, bindingElements.getLength());
         final NodeList policyElements = result.getElementsByTagNameNS("http://www.w3.org/ns/ws-policy", "Policy");
         assertEquals(4, policyElements.getLength());
+    }
+
+    public void testBridgeWsdlRemovePolicies() throws Exception {
+        final HashMap<URI, Policy> urnToPolicy = new HashMap<URI, Policy>();
+        final ManagementWSDLPatcher instance = new ManagementWSDLPatcher(urnToPolicy);
+        final StringReader reader = new StringReader(WSDL);
+        final XMLInputFactory inputFactory = XMLInputFactory.newInstance();
+        final XMLStreamReader xmlReader = inputFactory.createXMLStreamReader(reader);
+        final StringWriter writer = new StringWriter();
+        final XMLOutputFactory outputFactory = XMLOutputFactory.newInstance();
+        final XMLStreamWriter xmlWriter = outputFactory.createXMLStreamWriter(writer);
+        instance.bridge(xmlReader, xmlWriter);
+        xmlWriter.flush();
+
+        final Document result = builder.parse(new InputSource(new StringReader(writer.toString())));
+        final NodeList bindingElements = result.getElementsByTagNameNS("http://schemas.xmlsoap.org/wsdl/", "binding");
+        assertEquals(1, bindingElements.getLength());
+        final NodeList policyElements = result.getElementsByTagNameNS("http://www.w3.org/ns/ws-policy", "Policy");
+        assertEquals(0, policyElements.getLength());
+    }
+
+    public void testBridgeWsdlRemoveAndAddPolicies() throws Exception {
+        final HashMap<URI, Policy> urnToPolicy = new HashMap<URI, Policy>();
+        urnToPolicy.put(ExternalAttachmentsUnmarshaller.BINDING_ID, Policy.createEmptyPolicy(null, "binding-policy"));
+        urnToPolicy.put(ExternalAttachmentsUnmarshaller.BINDING_OPERATION_ID, Policy.createEmptyPolicy(null, "operation-policy"));
+        urnToPolicy.put(ExternalAttachmentsUnmarshaller.BINDING_OPERATION_INPUT_ID, Policy.createEmptyPolicy(null, "input-policy"));
+        urnToPolicy.put(ExternalAttachmentsUnmarshaller.BINDING_OPERATION_OUTPUT_ID, Policy.createEmptyPolicy(null, "output-policy"));
+        urnToPolicy.put(ExternalAttachmentsUnmarshaller.BINDING_OPERATION_FAULT_ID, Policy.createEmptyPolicy(null, "fault-policy"));
+        final ManagementWSDLPatcher instance = new ManagementWSDLPatcher(urnToPolicy);
+        final StringReader reader = new StringReader(WSDL_NO_POLICY);
+        final XMLInputFactory inputFactory = XMLInputFactory.newInstance();
+        final XMLStreamReader xmlReader = inputFactory.createXMLStreamReader(reader);
+        final StringWriter writer = new StringWriter();
+        final XMLOutputFactory outputFactory = XMLOutputFactory.newInstance();
+        final XMLStreamWriter xmlWriter = outputFactory.createXMLStreamWriter(writer);
+        instance.bridge(xmlReader, xmlWriter);
+        xmlWriter.flush();
+
+        final Document result = builder.parse(new InputSource(new StringReader(writer.toString())));
+        final NodeList bindingElements = result.getElementsByTagNameNS("http://schemas.xmlsoap.org/wsdl/", "binding");
+        assertEquals(1, bindingElements.getLength());
+        final NodeList policyElements = result.getElementsByTagNameNS("http://www.w3.org/ns/ws-policy", "Policy");
+        assertEquals(4, policyElements.getLength());
+        final NodeList policyReferenceElements = result.getElementsByTagNameNS("http://www.w3.org/ns/ws-policy", "PolicyReference");
+        assertEquals(0, policyReferenceElements.getLength());
     }
 
 }
