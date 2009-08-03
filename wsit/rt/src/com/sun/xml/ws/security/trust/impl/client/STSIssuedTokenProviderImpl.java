2572452d40a1fbcb4b0057b2a0308e025c2e42ba
jdg6688 <jdg6688@localhost>
2009-08-03 22:27:02 +0000

    Managing expired token on the client side.
    svn path=/trunk/; revision=5271
    
    
    Former-commit-id: daaaa9d46e6c94d501d8133a20b99df68ea81bd4

--- wsit/rt/src/com/sun/xml/ws/security/trust/impl/client/STSIssuedTokenProviderImpl.java
+++ wsit/rt/src/com/sun/xml/ws/security/trust/impl/client/STSIssuedTokenProviderImpl.java
@@ -43,39 +43,34 @@ import com.sun.xml.ws.api.security.trust.WSTrustException;
 import com.sun.xml.ws.api.security.trust.client.IssuedTokenProvider;
 import com.sun.xml.ws.api.security.trust.client.STSIssuedTokenConfiguration;
 import com.sun.xml.wss.SubjectAccessor;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Set;
 import javax.security.auth.Subject;
+
+import com.sun.xml.ws.security.trust.logging.LogDomainConstants;
+import com.sun.xml.ws.security.trust.logging.LogStringsMessages;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Jiandong Guo
  */
 public class STSIssuedTokenProviderImpl implements IssuedTokenProvider {
+
+     private static final Logger log =
+            Logger.getLogger(
+            LogDomainConstants.TRUST_IMPL_DOMAIN,
+            LogDomainConstants.TRUST_IMPL_DOMAIN_BUNDLE);
     
     public void issue(IssuedTokenContext ctx)throws WSTrustException{
         STSIssuedTokenConfiguration config = (STSIssuedTokenConfiguration)ctx.getSecurityPolicy().get(0);
         ctx.setTokenIssuer(config.getSTSEndpoint());
         boolean shareToken = "true".equals(config.getOtherOptions().get(STSIssuedTokenConfiguration.SHARE_TOKEN));
-        Subject subject = SubjectAccessor.getRequesterSubject();
-        if (shareToken && subject != null){
-            Set pcs = subject.getPrivateCredentials(IssuedTokenContext.class);
-            for (Object obj : pcs){
-                IssuedTokenContext cached = (IssuedTokenContext)obj;
-                if (cached.getTokenIssuer().equals(ctx.getTokenIssuer())){
-                    updateContext(cached, ctx);
-                    return;
-                }
-            }
-        }
-        TrustPlugin tp = WSTrustFactory.newTrustPlugin();
-        tp.process(ctx);
-        if (shareToken){
-            if (subject == null){
-                subject = new Subject();
-            }
-            subject.getPrivateCredentials().add(ctx);
-            SubjectAccessor.setRequesterSubject(subject);
-        }
+        getIssuedTokenContext(shareToken, ctx);
     } 
     
     public void cancel(IssuedTokenContext ctx)throws WSTrustException{
@@ -100,5 +95,49 @@ public class STSIssuedTokenProviderImpl implements IssuedTokenProvider {
         ctx.setExpirationTime(cached.getExpirationTime());
         ctx.setCreationTime(cached.getCreationTime());
         ctx.setAttachedSecurityTokenReference(cached.getAttachedSecurityTokenReference());
+    }
+
+    private void getIssuedTokenContext(boolean shareToken, IssuedTokenContext ctx)throws WSTrustException {
+        Subject subject = SubjectAccessor.getRequesterSubject();
+        if (shareToken && subject != null){
+            Set pcs = subject.getPrivateCredentials(IssuedTokenContext.class);
+            for (Object obj : pcs){
+                IssuedTokenContext cached = (IssuedTokenContext)obj;
+
+                // Check if the token is expired
+                Calendar c = new GregorianCalendar();
+                long offset = c.get(Calendar.ZONE_OFFSET);
+                if (c.getTimeZone().inDaylightTime(c.getTime())) {
+                    offset += c.getTimeZone().getDSTSavings();
+                }
+                long beforeTime = c.getTimeInMillis();
+                long currentTime = beforeTime - offset;
+                c.setTimeInMillis(currentTime);
+                Date currentTimeInDateFormat = c.getTime();
+                if(!currentTimeInDateFormat.before(ctx.getExpirationTime())){
+                    // Remove expired the context
+                    subject.getPrivateCredentials().remove(cached);
+
+                    // ToDo: renew the expired token if required
+                    
+                    log.log(Level.SEVERE,
+                    LogStringsMessages.WST_0046_TOKEN_EXPIRED(ctx.getCreationTime(), ctx.getExpirationTime(), currentTimeInDateFormat));
+                    throw new WSTrustException(LogStringsMessages.WST_0046_TOKEN_EXPIRED(ctx.getCreationTime(), ctx.getExpirationTime(), currentTimeInDateFormat));
+                } else if (cached.getTokenIssuer().equals(ctx.getTokenIssuer())){
+                    updateContext(cached, ctx);
+                    return;
+                }
+            }
+        }
+        
+        TrustPlugin tp = WSTrustFactory.newTrustPlugin();
+        tp.process(ctx);
+        if (shareToken){
+            if (subject == null){
+                 subject = new Subject();
+            }
+            subject.getPrivateCredentials().add(ctx);
+            SubjectAccessor.setRequesterSubject(subject);
+        }
     }
 }
