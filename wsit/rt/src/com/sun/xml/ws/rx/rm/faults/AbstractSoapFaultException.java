3847ea5b492ca0dd194b0af050027858c46200fd
Marek Potociar <marek.potociar@oracle.com>
2010-01-13 16:23:02 +0000

    removed an obsolete TODO comment
    svn path=/trunk/; revision=5778
    
    
    Former-commit-id: 85a4d4bf3fc011d61bbc06299e366b70b4482a69

--- wsit/rt/src/com/sun/xml/ws/rx/rm/faults/AbstractSoapFaultException.java
+++ wsit/rt/src/com/sun/xml/ws/rx/rm/faults/AbstractSoapFaultException.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  * 
- * Copyright 1997-2008 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2010 Sun Microsystems, Inc. All rights reserved.
  * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -115,7 +115,6 @@ public abstract class AbstractSoapFaultException extends RxRuntimeException {
 
     protected final Message createSoapFaultMessage(RuntimeContext rc, boolean attachSequenceFaultElement) {
         try {
-            // TODO P1 where to set soap fault code?
             SOAPFault soapFault = rc.soapVersion.saajSoapFactory.createFault();
 
             // common SOAP1.1 and SOAP1.2 Fault settings
