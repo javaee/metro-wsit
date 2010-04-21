1179152cc4012a3e88decb44d21a366d1cae9267
Fabian Ritzmann <ritzmann@java.net>
2010-04-21 12:37:24 +0000

    Implemented performance improvement suggested by Findbugs.
    svn path=/trunk/; revision=5953
    
    
    Former-commit-id: 53b1fce1fe05fd65108a3d824068e5fe1db11ff4

--- wsit/rt/src/com/sun/xml/ws/config/management/jmx/JMXAgent.java
+++ wsit/rt/src/com/sun/xml/ws/config/management/jmx/JMXAgent.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2009 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2010 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -234,8 +234,8 @@ public class JMXAgent<T> implements CommunicationServer<T> {
                             final Map<QName, String> parameters = parameter.getParameters();
                             final Map<String, String> result = new HashMap<String, String>();
                             if (parameters != null) {
-                                for (QName key : parameters.keySet()) {
-                                    result.put(key.getLocalPart(), parameters.get(key));
+                                for (Map.Entry<QName, String> entry : parameters.entrySet()) {
+                                    result.put(entry.getKey().getLocalPart(), entry.getValue());
                                 }
                             }
                             return result;
