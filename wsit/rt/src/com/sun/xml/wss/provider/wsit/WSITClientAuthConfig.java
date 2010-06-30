a5a8e3a6e3d30425874f290eb04f393802e70ef0
sm228678 <sm228678@localhost>
2010-06-30 11:59:17 +0000

    modified fix for Memory Leak Bug #1260: updated the copy right headers
    svn path=/trunk/; revision=6101
    
    
    Former-commit-id: e726835966ba72e6c89fb6fe0aaef156e65f3615

--- wsit/rt/src/com/sun/xml/wss/provider/wsit/WSITClientAuthConfig.java
+++ wsit/rt/src/com/sun/xml/wss/provider/wsit/WSITClientAuthConfig.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  * 
- * Copyright 1997-2008 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2010 Sun Microsystems, Inc. All rights reserved.
  * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -183,7 +183,7 @@ public class WSITClientAuthConfig implements ClientAuthConfig {
         return true;
     }
 
-    public ClientAuthContext cleanupAuthContext(Object  hashCode) {
+    public ClientAuthContext cleanupAuthContext(Integer hashCode) {
         return this.tubetoClientAuthContextHash.remove( hashCode);
     }
 
