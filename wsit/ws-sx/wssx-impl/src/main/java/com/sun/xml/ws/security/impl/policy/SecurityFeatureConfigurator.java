4bad945cc02c78f420602818f2654157f82f6e0f
Marek Potociar <marek.potociar@oracle.com>
2010-10-18 18:09:38 +0000

    Finalized initial drop of stickiness and replica id propagation support in SC and NonceManager
    svn path=/trunk/; revision=6463
    
    
    Former-commit-id: fa13012bbf6a7838345cda4c4d038bee456df944

---
+++ wsit/ws-sx/wssx-impl/src/main/java/com/sun/xml/ws/security/impl/policy/SecurityFeatureConfigurator.java
@@ -0,0 +1,150 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * 
+ * Copyright (c) 2010 Oracle and/or its affiliates. All rights reserved.
+ * 
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ * 
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ * 
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ * 
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ * 
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+
+package com.sun.xml.ws.security.impl.policy;
+
+import com.sun.xml.ws.api.ha.StickyFeature;
+import com.sun.xml.ws.policy.AssertionSet;
+import com.sun.xml.ws.policy.Policy;
+import com.sun.xml.ws.policy.PolicyAssertion;
+import com.sun.xml.ws.policy.PolicyException;
+import com.sun.xml.ws.policy.PolicyMap;
+import com.sun.xml.ws.policy.PolicyMapKey;
+import com.sun.xml.ws.policy.jaxws.spi.PolicyFeatureConfigurator;
+import com.sun.xml.ws.security.policy.SecurityPolicyVersion;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import javax.xml.namespace.QName;
+import javax.xml.ws.WebServiceFeature;
+
+/**
+ * Policy WS feature configurator implementation for the security domain
+ *
+ * @author Marek Potociar (marek.potociar at sun.com)
+ */
+public final class SecurityFeatureConfigurator implements PolicyFeatureConfigurator {
+    /*
+     * Empty marker class that is used to tell JAX-WS RI that client session
+     * stickiness should be enabled.
+     * <p />
+     * This is used whenever we detect that NonceManager or SC was enabled.
+     *
+     */
+    private static final class MarkerStickyFeature extends WebServiceFeature implements StickyFeature {
+        private static final String ID = MarkerStickyFeature.class.getName();
+
+        @Override
+        public String getID() {
+            return ID;
+        }
+
+    }
+
+    public Collection<WebServiceFeature> getFeatures(PolicyMapKey key, PolicyMap policyMap) throws PolicyException {
+        boolean stickinessDetected = false;
+        final Collection<WebServiceFeature> features = new LinkedList<WebServiceFeature>();
+        if ((key != null) && (policyMap != null)) {
+            Policy policy = policyMap.getEndpointEffectivePolicy(key);
+            if (policy != null) {
+                for (AssertionSet alternative : policy) {
+                    stickinessDetected = resolveStickiness(alternative.iterator());
+
+                    List<WebServiceFeature> singleAlternativeFeatures;
+                    singleAlternativeFeatures = getFeatures(alternative);
+                    if (!singleAlternativeFeatures.isEmpty()) {
+                        features.addAll(singleAlternativeFeatures);
+                    }
+                }
+            } // end-if policy not null
+        }
+
+        if (stickinessDetected) {
+           features.add(new MarkerStickyFeature());
+        }
+
+        return features;
+    }
+
+    /**
+     * NonceManager is used when there is sp:UsernameToken assertion in Policy of the Service with DigestAuthentication enabled.
+     * SC feature is enabled by having a sp:SecureConversationToken in the Policy of the Service.
+     */
+    private static final Set<QName> STICKINESS_ENABLERS = Collections.unmodifiableSet(new HashSet(Arrays.asList(new QName[] {
+        new QName(SecurityPolicyVersion.SECURITYPOLICY200507.namespaceUri, "SecureConversationToken"),
+        new QName(SecurityPolicyVersion.SECURITYPOLICY12NS.namespaceUri, "SecureConversationToken"),
+
+        new QName(SecurityPolicyVersion.SECURITYPOLICY200507.namespaceUri, "UsernameToken"),
+        new QName(SecurityPolicyVersion.SECURITYPOLICY12NS.namespaceUri, "UsernameToken")
+    })));
+
+    /**
+     *
+     */
+    private boolean resolveStickiness(Iterator<PolicyAssertion> assertions) {
+        while(assertions.hasNext()) {
+            final PolicyAssertion assertion = assertions.next();
+            if (STICKINESS_ENABLERS.contains(assertion.getName())) {
+                return true;
+            }
+
+            if (assertion.hasParameters() && resolveStickiness(assertion.getParametersIterator())) {
+                return true;
+            }
+
+            if (assertion.hasNestedPolicy() && resolveStickiness(assertion.getNestedPolicy().getAssertionSet().iterator())) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private List<WebServiceFeature> getFeatures(AssertionSet alternative) {
+        // method will be useful in the future with unified config; do nothing for now
+        return Collections.emptyList();
+    }
+
+}
