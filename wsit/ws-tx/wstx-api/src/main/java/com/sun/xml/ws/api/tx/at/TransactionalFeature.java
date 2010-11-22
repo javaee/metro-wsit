4eaea2596cc7b2bc12c88a7a510764b3f984680b
Marek Potociar <marek.potociar@oracle.com>
2010-11-22 17:36:18 +0000

    Enhanced constructor invocation logging
    svn path=/trunk/; revision=6578
    
    
    Former-commit-id: e010202f0a5e456391cef60a5f1197a068153b44

--- wsit/ws-tx/wstx-api/src/main/java/com/sun/xml/ws/api/tx/at/TransactionalFeature.java
+++ wsit/ws-tx/wstx-api/src/main/java/com/sun/xml/ws/api/tx/at/TransactionalFeature.java
@@ -37,9 +37,9 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-
 package com.sun.xml.ws.api.tx.at;
 
+import com.sun.istack.logging.Logger;
 import com.sun.xml.ws.api.FeatureConstructor;
 
 import javax.xml.ws.WebServiceFeature;
@@ -59,171 +59,176 @@ import java.util.Map;
  * </ul>
  * <p/>
  */
-public class TransactionalFeature extends WebServiceFeature{
-  /**
-   * Constant value identifying the TransactionalFeature
-   */
-  public static final String ID = "http://docs.oasis-open.org/ws-tx/";
+public class TransactionalFeature extends WebServiceFeature {
 
-  private Transactional.TransactionFlowType flowType = Transactional.TransactionFlowType.SUPPORTS;
-  private boolean isExplicitMode;
-  private Transactional.Version version = Transactional.Version.DEFAULT;
-  private Map<String, Transactional.TransactionFlowType> flowTypeMap = new HashMap<String, Transactional.TransactionFlowType>();
-  private Map<String, Boolean> enabledMap = new HashMap<String, Boolean>();
+    private static final Logger LOGGER = Logger.getLogger(TransactionalFeature.class);
+    /**
+     * Constant value identifying the TransactionalFeature
+     */
+    public static final String ID = "http://docs.oasis-open.org/ws-tx/";
+    private Transactional.TransactionFlowType flowType = Transactional.TransactionFlowType.SUPPORTS;
+    private boolean isExplicitMode;
+    private Transactional.Version version = Transactional.Version.DEFAULT;
+    private Map<String, Transactional.TransactionFlowType> flowTypeMap = new HashMap<String, Transactional.TransactionFlowType>();
+    private Map<String, Boolean> enabledMap = new HashMap<String, Boolean>();
 
-  @FeatureConstructor({"enabled", "value", "version"})
-  public TransactionalFeature(boolean enabled, Transactional.TransactionFlowType value, Transactional.Version version) {
-    this.enabled = enabled;
-    this.flowType = value;
-    this.version = version;
-  }
+    @FeatureConstructor({"enabled", "value", "version"})
+    public TransactionalFeature(boolean enabled, Transactional.TransactionFlowType value, Transactional.Version version) {
+        LOGGER.entering(enabled, value, version);
+        
+        this.enabled = enabled;
+        this.flowType = value;
+        this.version = version;
+    }
 
-  /**
-   * Create an <code>TransactionalFeature</code>.
-   * The instance created will be enabled.
-   */
+    /**
+     * Create an <code>TransactionalFeature</code>.
+     * The instance created will be enabled.
+     */
+    public TransactionalFeature() {
+        LOGGER.entering();
+        
+        this.enabled = true;
+    }
 
-  public TransactionalFeature() {
-    this.enabled = true;
-  }
+    /**
+     * Create an <code>TransactionalFeature</code>
+     *
+     * @param enabled specifies whether this feature should
+     *                be enabled or not.
+     */
+    public TransactionalFeature(boolean enabled) {
+        LOGGER.entering(enabled);
+        
+        this.enabled = enabled;
+    }
 
-  /**
-   * Create an <code>TransactionalFeature</code>
-   *
-   * @param enabled specifies whether this feature should
-   *                be enabled or not.
-   */
-  public TransactionalFeature(boolean enabled) {
-    this.enabled = enabled;
-  }
+    /**
+     * Returns the default Transaction flow type for all operations.
+     * @return Transactional.TransactionFlowType
+     */
+    public Transactional.TransactionFlowType getFlowType() {
+        return flowType;
+    }
 
-  /**
-   * Returns the default Transaction flow type for all operations.
-   * @return Transactional.TransactionFlowType
-   */
-  public Transactional.TransactionFlowType getFlowType() {
-    return flowType;
-  }
+    /**
+     * Returns the Transaction flow type for a given operation.
+     * @return Transactional.TransactionFlowType
+     */
+    public Transactional.TransactionFlowType getFlowType(String operationName) {
+        Transactional.TransactionFlowType type = flowTypeMap.get(operationName);
+        if (!isExplicitMode && type == null) {
+            type = flowType;
+        }
+        return type;
+    }
 
-  /**
-   * Returns the Transaction flow type for a given operation.
-   * @return Transactional.TransactionFlowType
-   */
-  public Transactional.TransactionFlowType getFlowType(String operationName) {
-    Transactional.TransactionFlowType type = flowTypeMap.get(operationName);
-    if (!isExplicitMode && type == null)
-      type = flowType;
-    return type;
-  }
+    /**
+     * Set the default Transaction flow type for all operations.
+     * @param flowType
+     */
+    public void setFlowType(Transactional.TransactionFlowType flowType) {
+        this.flowType = flowType;
+    }
 
-  /**
-   * Set the default Transaction flow type for all operations.
-   * @param flowType
-   */
-  public void setFlowType(Transactional.TransactionFlowType flowType) {
-    this.flowType = flowType;
-  }
+    /**
+     * Set the Transaction flow type for a given wsdl:operation.
+     * @param operationName  the local part of wsdl:opration
+     * @param flowType Transaction flow type
+     */
+    public void setFlowType(String operationName, Transactional.TransactionFlowType flowType) {
+        flowTypeMap.put(operationName, flowType);
+    }
 
-  /**
-   * Set the Transaction flow type for a given wsdl:operation.
-   * @param operationName  the local part of wsdl:opration
-   * @param flowType Transaction flow type
-   */
-  public void setFlowType(String operationName, Transactional.TransactionFlowType flowType) {
-    flowTypeMap.put(operationName, flowType);
-  }
+    public String getID() {
+        return ID;
+    }
 
+    /**
+     * Enable/disable this feature at port level
+     *
+     * @param enabled
+     */
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
 
-  public String getID() {
-    return ID;
-  }
+    /**
+     * Enable/disable this feature on a given operation
+     *
+     * @param operationName the local part of operation.
+     * @param enabled
+     */
+    public void setEnabled(String operationName, boolean enabled) {
+        enabledMap.put(operationName, enabled);
+    }
 
-  /**
-   * Enable/disable this feature at port level
-   *
-   * @param enabled
-   */
-  public void setEnabled(boolean enabled) {
-    this.enabled = enabled;
-  }
+    /**
+     * Returns <code>true</code> if WS-AT is enabled on the given operation.
+     *
+     * @param operationName  the local part of wsdl:operation
+     * @return <code>true</code> if and only if the WS-AT is enabled on the given operation.
+     */
+    public boolean isEnabled(String operationName) {
+        Boolean isEnabled = enabledMap.get(operationName);
+        if (isEnabled == null) {
+            return isExplicitMode ? false : enabled;
+        } else {
+            return isEnabled;
+        }
+    }
 
-  /**
-   * Enable/disable this feature on a given operation
-   *
-   * @param operationName the local part of operation.
-   * @param enabled
-   */
-  public void setEnabled(String operationName, boolean enabled) {
-    enabledMap.put(operationName, enabled);
-  }
+    /**
+     * Returns the version of WS-AT to be used.
+     *
+     * @return  Transactional.Version
+     */
+    public Transactional.Version getVersion() {
+        return version;
+    }
 
-  /**
-   * Returns <code>true</code> if WS-AT is enabled on the given operation.
-   *
-   * @param operationName  the local part of wsdl:operation
-   * @return <code>true</code> if and only if the WS-AT is enabled on the given operation.
-   */
-  public boolean isEnabled(String operationName) {
-    Boolean isEnabled = enabledMap.get(operationName);
-    if (isEnabled == null) {
-      return isExplicitMode ? false : enabled;
-    } else
-      return isEnabled;
-  }
+    /**
+     * Set the version of WS-AT to be used.
+     *
+     * @param version   the version of WS-AT to be used.
+     */
+    public void setVersion(Transactional.Version version) {
+        this.version = version;
+    }
 
+    /**
+     *  return a map listing the Transactional flow options for operations.
+     * @return a mapping listing the Transactional flow options explicit on operations.
+     */
+    public Map<String, Transactional.TransactionFlowType> getFlowTypeMap() {
+        return flowTypeMap;
+    }
 
-  /**
-   * Returns the version of WS-AT to be used.
-   *
-   * @return  Transactional.Version
-   */
-  public Transactional.Version getVersion() {
-    return version;
-  }
+    /**
+     *
+     * @return a mapping listing the transactional enabled attributes explicitly set on operations.
+     */
+    public Map<String, Boolean> getEnabledMap() {
+        return enabledMap;
+    }
 
-  /**
-   * Set the version of WS-AT to be used.
-   *
-   * @param version   the version of WS-AT to be used.
-   */
-  public void setVersion(Transactional.Version version) {
-    this.version = version;
-  }
+    /**
+     * Transactional Feature has two modes, explicit Mode or implicit Mode.
+     * In the implicit Mode, the Transactional Feature can be enabled at port level
+     *  and be inherited or override at operation level. In the explicit Mode,
+     *  transactional flow option can only specified and enabled at operation level.
+     *  the default is explicit Mode.
+     * @return whether this Transactional Feature is in explicit mode.
+     */
+    public boolean isExplicitMode() {
+        return isExplicitMode;
+    }
 
-  /**
-   *  return a map listing the Transactional flow options for operations.
-    * @return a mapping listing the Transactional flow options explicit on operations.
-   */
-  public Map<String, Transactional.TransactionFlowType> getFlowTypeMap() {
-    return flowTypeMap;
-  }
-
-  /**
-   *
-    * @return a mapping listing the transactional enabled attributes explicitly set on operations.
-   */
-  public Map<String, Boolean> getEnabledMap() {
-    return enabledMap;
-  }
-
-  /**
-   * Transactional Feature has two modes, explicit Mode or implicit Mode.
-   * In the implicit Mode, the Transactional Feature can be enabled at port level
-   *  and be inherited or override at operation level. In the explicit Mode,
-   *  transactional flow option can only specified and enabled at operation level.
-   *  the default is explicit Mode.
-   * @return whether this Transactional Feature is in explicit mode.
-   */
-  public boolean isExplicitMode() {
-    return isExplicitMode;
-  }
-
-  /**
-   * Change the Transactional Feature mode
-   * @param explicitMode whether set to explicit Mode.
-   */
-  public void setExplicitMode(boolean explicitMode) {
-    isExplicitMode = explicitMode;
-  }
-
+    /**
+     * Change the Transactional Feature mode
+     * @param explicitMode whether set to explicit Mode.
+     */
+    public void setExplicitMode(boolean explicitMode) {
+        isExplicitMode = explicitMode;
+    }
 }
