93830cb5f7bce19f1fef576737de1369cf693e23
Fabian Ritzmann <ritzmann@java.net>
2010-11-24 10:13:51 +0000

    WSIT-1456: Got rid of com.sun.xml.ws.policy package in WSIT workspace. Integrating policy 2.2.2-b04.
    svn path=/trunk/; revision=6592
    
    
    Former-commit-id: be31a9baed5b1afbccb56cf85701c74d4c0598c5

--- wsit/metro-config/metro-config-impl/src/main/java/com/sun/xml/ws/policy/parser/WsitPolicyResolver.java
+++ wsit/metro-config/metro-config-impl/src/main/java/com/sun/xml/ws/policy/parser/WsitPolicyResolver.java
@@ -41,17 +41,27 @@
 package com.sun.xml.ws.policy.parser;
 
 import com.sun.istack.logging.Logger;
+import com.sun.xml.ws.api.policy.AlternativeSelector;
 import com.sun.xml.ws.api.policy.PolicyResolver;
 import com.sun.xml.ws.api.policy.PolicyResolverFactory;
+import com.sun.xml.ws.api.policy.ValidationProcessor;
+import com.sun.xml.ws.policy.AssertionSet;
+import com.sun.xml.ws.policy.EffectivePolicyModifier;
+import com.sun.xml.ws.policy.Policy;
+import com.sun.xml.ws.policy.PolicyAssertion;
 import com.sun.xml.ws.policy.PolicyMap;
 import com.sun.xml.ws.policy.PolicyException;
 import com.sun.xml.ws.policy.PolicyConstants;
+import com.sun.xml.ws.policy.PolicyMapExtender;
+import com.sun.xml.ws.policy.PolicyMapKey;
 import com.sun.xml.ws.policy.PolicyMapMutator;
-import com.sun.xml.ws.policy.WsitPolicyUtil;
+import com.sun.xml.ws.policy.PolicySubject;
 import com.sun.xml.ws.policy.localization.LocalizationMessages;
+import com.sun.xml.ws.policy.spi.PolicyAssertionValidator.Fitness;
 
 import java.net.URL;
 import java.util.Collection;
+import javax.xml.stream.FactoryConfigurationError;
 import javax.xml.ws.WebServiceException;
 
 /**
@@ -84,7 +94,7 @@ public class WsitPolicyResolver implements PolicyResolver {
                 LOGGER.config(LocalizationMessages.WSP_5008_CREATE_POLICY_MAP_FOR_CONFIG(configId));
             else {
                 // Validate server-side Policies such that there exists a single alternative in each scope.
-                WsitPolicyUtil.validateServerPolicyMap(map);
+                validateServerPolicyMap(map);
             }
             return map;
         }
@@ -114,7 +124,7 @@ public class WsitPolicyResolver implements PolicyResolver {
                 effectivePolicyMap = context.getPolicyMap();
             } else {
                 //Merge Policy Configuration from WSDL and configuration file.
-                effectivePolicyMap = WsitPolicyUtil.mergePolicyMap(context.getPolicyMap(), clientConfigPolicyMap);
+                effectivePolicyMap = mergePolicyMap(context.getPolicyMap(), clientConfigPolicyMap);
             }
         } catch (PolicyException e) {
             throw LOGGER.logSevereException(new WebServiceException(
@@ -122,8 +132,118 @@ public class WsitPolicyResolver implements PolicyResolver {
         }
         // Chooses best alternative and sets it as effective Policy in each scope.
         if(effectivePolicyMap != null)
-            return WsitPolicyUtil.doAlternativeSelection(effectivePolicyMap);
+            return doAlternativeSelection(effectivePolicyMap);
         else
             return null;
     }
+
+     /**
+     * Checks if the PolicyMap has only single alternative in the scope.
+     *
+     * @param policyMap
+     *      PolicyMap that needs to be validated.
+     */
+    private static void validateServerPolicyMap(PolicyMap policyMap) {
+        try {
+            final ValidationProcessor validationProcessor = ValidationProcessor.getInstance();
+
+            for (Policy policy : policyMap) {
+
+                // TODO:  here is a good place to check if the actual policy has only one alternative...
+
+                for (AssertionSet assertionSet : policy) {
+                    for (PolicyAssertion assertion : assertionSet) {
+                        Fitness validationResult = validationProcessor.validateServerSide(assertion);
+                        if (validationResult != Fitness.SUPPORTED) {
+                            throw new PolicyException(LocalizationMessages.WSP_5017_SERVER_SIDE_ASSERTION_VALIDATION_FAILED(
+                                    assertion.getName(),
+                                    validationResult));
+                        }
+                    }
+                }
+            }
+        } catch (PolicyException e) {
+            throw new WebServiceException(e);
+        }
+    }
+
+    /**
+     * Selects a best alternative if there are multiple policy alternatives.
+     *
+     * @param policyMap
+     * @return
+     */
+    private static PolicyMap doAlternativeSelection(PolicyMap policyMap) {
+        final EffectivePolicyModifier modifier = EffectivePolicyModifier.createEffectivePolicyModifier();
+        modifier.connect(policyMap);
+        try {
+            AlternativeSelector.doSelection(modifier);
+        } catch (PolicyException e) {
+            throw new WebServiceException(e);
+        } finally {
+            modifier.disconnect();
+        }
+        return policyMap;
+    }
+
+    /**
+     * Merge Policies policyMap and clientPolicyMap.
+     *
+     * @param policyMap The first policy map to be merged.
+     * @param clientPolicyMap The second policy map to be merged.
+     * @return merged PolicyMap
+     * @throws PolicyException If merge failed.
+     */
+    private static PolicyMap mergePolicyMap(PolicyMap policyMap, PolicyMap clientPolicyMap) throws PolicyException {
+        final PolicyMapExtender mapExtender = PolicyMapExtender.createPolicyMapExtender();
+        final String clientWsitConfigId = PolicyConstants.CLIENT_CONFIGURATION_IDENTIFIER;
+        if (policyMap != null) {
+            mapExtender.connect(policyMap);
+            try {
+                for (PolicyMapKey key : clientPolicyMap.getAllServiceScopeKeys()) {
+                    final Policy policy = clientPolicyMap.getServiceEffectivePolicy(key);
+                    // setting subject to provided URL of client WSIT config
+                    mapExtender.putServiceSubject(key, new PolicySubject(clientWsitConfigId, policy));
+                }
+
+                for (PolicyMapKey key : clientPolicyMap.getAllEndpointScopeKeys()) {
+                    final Policy policy = clientPolicyMap.getEndpointEffectivePolicy(key);
+                    // setting subject to provided URL of client WSIT config
+                    mapExtender.putEndpointSubject(key, new PolicySubject(clientWsitConfigId, policy));
+                }
+
+                for (PolicyMapKey key : clientPolicyMap.getAllOperationScopeKeys()) {
+                    final Policy policy = clientPolicyMap.getOperationEffectivePolicy(key);
+                    // setting subject to provided URL of client WSIT config
+                    mapExtender.putOperationSubject(key, new PolicySubject(clientWsitConfigId, policy));
+                }
+
+                for (PolicyMapKey key : clientPolicyMap.getAllInputMessageScopeKeys()) {
+                    final Policy policy = clientPolicyMap.getInputMessageEffectivePolicy(key);
+                    // setting subject to provided URL of client WSIT config
+                    mapExtender.putInputMessageSubject(key, new PolicySubject(clientWsitConfigId, policy));
+                }
+
+                for (PolicyMapKey key : clientPolicyMap.getAllOutputMessageScopeKeys()) {
+                    final Policy policy = clientPolicyMap.getOutputMessageEffectivePolicy(key);
+                    // setting subject to provided URL of client WSIT config
+                    mapExtender.putOutputMessageSubject(key, new PolicySubject(clientWsitConfigId, policy));
+                }
+
+                for (PolicyMapKey key : clientPolicyMap.getAllFaultMessageScopeKeys()) {
+                    final Policy policy = clientPolicyMap.getFaultMessageEffectivePolicy(key);
+                    // setting subject to provided URL of client WSIT config
+                    mapExtender.putFaultMessageSubject(key, new PolicySubject(clientWsitConfigId, policy));
+                }
+                LOGGER.fine(LocalizationMessages.WSP_5015_CLIENT_CFG_POLICIES_TRANSFERED_INTO_FINAL_POLICY_MAP(policyMap));
+            } catch (FactoryConfigurationError ex) {
+                throw LOGGER.logSevereException(new PolicyException(ex));
+            }
+            return policyMap;
+
+        } else {
+            return clientPolicyMap;
+        }
+    }
+
 }
