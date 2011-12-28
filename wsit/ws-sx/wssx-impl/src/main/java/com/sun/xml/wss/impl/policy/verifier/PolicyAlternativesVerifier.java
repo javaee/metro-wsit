2951c291b44de5d99a9fa5a2d4a1497ff430a773
Kumar Jayanti <v.b.kumar.jayanti@oracle.com>
2011-12-28 12:54:33 +0000

    Fix for issue 1584
    svn path=/trunk/; revision=6934
    
    
    Former-commit-id: e83d95dedda3f91b3e0d15f52988750cddf82bad

--- wsit/ws-sx/wssx-impl/src/main/java/com/sun/xml/wss/impl/policy/verifier/PolicyAlternativesVerifier.java
+++ wsit/ws-sx/wssx-impl/src/main/java/com/sun/xml/wss/impl/policy/verifier/PolicyAlternativesVerifier.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -40,16 +40,17 @@
 
 package com.sun.xml.wss.impl.policy.verifier;
 
+import com.sun.xml.ws.security.spi.AlternativeSelector;
 import com.sun.xml.wss.ProcessingContext;
-import com.sun.xml.wss.impl.PolicyTypeUtil;
 import com.sun.xml.wss.impl.PolicyViolationException;
 import com.sun.xml.wss.impl.policy.PolicyAlternatives;
 import com.sun.xml.wss.impl.policy.SecurityPolicy;
 import com.sun.xml.wss.impl.policy.mls.MessagePolicy;
-import com.sun.xml.wss.impl.policy.mls.WSSPolicy;
 import com.sun.xml.wss.impl.policy.spi.PolicyVerifier;
 import com.sun.xml.wss.logging.LogDomainConstants;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ServiceLoader;
 import java.util.logging.Logger;
 
 /**
@@ -83,7 +84,8 @@ public class PolicyAlternativesVerifier implements PolicyVerifier {
         } else {
            //do policy verification 
            // try with an AlternativeSelector first
-           AlternativeSelector selector = new  DefaultAlternativeSelector();
+           //AlternativeSelector selector = new  DefaultAlternativeSelector();
+           AlternativeSelector selector = findAlternativesSelector(mps);
            MessagePolicy toVerify = selector.selectAlternative(ctx, mps, recvdPolicy);
            //TODO: the PolicyVerifier.verifyPolicy() method expects the toVerify argument to be
            //passed again. since that interface is a legacy interface,  not changing it
@@ -104,77 +106,29 @@ public class PolicyAlternativesVerifier implements PolicyVerifier {
         }
 
     }
-
-    //TODO: make the AlternativeSelector to be used configurable via META-INF/Services or as Classpath Resource ?.
-    //Or Have a Factory Method.
-    private static interface AlternativeSelector {
-        public MessagePolicy selectAlternative(ProcessingContext ctx, List<MessagePolicy> alternatives, SecurityPolicy recvdPolicy);
-    }
-
-    private static class DefaultAlternativeSelector  implements AlternativeSelector {
-
-        public MessagePolicy selectAlternative(ProcessingContext ctx, List<MessagePolicy> alternatives, SecurityPolicy recvdPolicy) {
-            MessagePolicy selected = null;
+    
+    private AlternativeSelector findAlternativesSelector(List<MessagePolicy> alternatives) {
+        ServiceLoader<AlternativeSelector> alternativeSelectorLoader = ServiceLoader.load(AlternativeSelector.class);
+        //not clear from javadoc if null is returned ever or an RT exception thrown when it does not find
+        // the services definitions.
+        if (alternativeSelectorLoader == null) {
             if (alternatives.size() == 2) {
-                selected = (new UsernameOrSAMLAlternativeSelector()).selectAlternative(ctx, alternatives, recvdPolicy);
+                return new UsernameOrSAMLAlternativeSelector();
+            } else {
+                throw new UnsupportedOperationException("No AlternativeSelector accepts the policy alternatives combination.");
             }
-            if (selected != null) {
-                return selected;
-            }
-            throw new UnsupportedOperationException(
-                    "Cannot verify the request against the configured PolicyAlternatives in the WebService");
         }
+        Iterator<AlternativeSelector> alternativeSelectorIterator = alternativeSelectorLoader.iterator();
+
+        while (alternativeSelectorIterator.hasNext()) {
+            AlternativeSelector selector = alternativeSelectorIterator.next();
+            if (selector.supportsAlternatives(alternatives)) {
+                return selector;
+            }
+        }
+
+        throw new UnsupportedOperationException("No AlternativeSelector accepts the policy alternatives combination.");
     }
 
-
-    /**
-     * A class which can select specific alternatives of a Username or a SAMLToken appearing
-     * as SignedSupportingTokens.
-     */
-    private static class UsernameOrSAMLAlternativeSelector implements AlternativeSelector {
-
-        private enum SupportingTokenType {USERNAME, SAML, UNKNOWN};
-
-        public MessagePolicy selectAlternative(ProcessingContext ctx, List<MessagePolicy> alternatives, SecurityPolicy recvdPolicy) {
-            //TODO: assert that the number of alternatives is two only
-            //it can handle the alternatives as defined in the following oracle security profiles :
-            //1. wss11_saml_or_username_token_with_message_protection_service_policy
-            //2. OR wss_saml_or_username_token_over_ssl_service_policy
-            SupportingTokenType reqMsgTokenType = determineTokenType(recvdPolicy);
-            for (MessagePolicy mp: alternatives) {
-                SupportingTokenType alternativeTokenType = determineTokenType(mp);
-                if (reqMsgTokenType !=SupportingTokenType.UNKNOWN &&
-                        reqMsgTokenType.equals(alternativeTokenType)) {
-                    return mp;
-                }
-            }
-            return null;
-        }
-
-        private SupportingTokenType determineTokenType(SecurityPolicy recvdPolicy) {
-            SupportingTokenType ret = SupportingTokenType.UNKNOWN;
-            if (recvdPolicy instanceof MessagePolicy) {
-                MessagePolicy pol = (MessagePolicy)recvdPolicy;
-                for (int i=0; i < pol.size(); i++) {
-                    try {
-                        WSSPolicy p =(WSSPolicy) pol.get(i);
-                        if (PolicyTypeUtil.usernameTokenBinding(p) ||
-                              PolicyTypeUtil.usernameTokenBinding(p.getFeatureBinding())) {
-                            ret = SupportingTokenType.USERNAME;
-                            break;
-                        } else if (PolicyTypeUtil.samlTokenPolicy(p) ||
-                                PolicyTypeUtil.samlTokenPolicy(p.getFeatureBinding())) {
-                            ret = SupportingTokenType.SAML;
-                            break;
-                        }
-                    } catch (Exception e) {
-                        //nothing to do.
-                    }
-                }
-            }
-            return ret;
-        }
-
-    }
 
 }
---
+++ wsit/ws-sx/wssx-impl/src/main/java/com/sun/xml/wss/impl/policy/verifier/UsernameOrX509AlternativeSelector.java
@@ -0,0 +1,121 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2011 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+package com.sun.xml.wss.impl.policy.verifier;
+
+import com.sun.xml.ws.security.spi.AlternativeSelector;
+import com.sun.xml.wss.ProcessingContext;
+import com.sun.xml.wss.impl.PolicyTypeUtil;
+import com.sun.xml.wss.impl.policy.SecurityPolicy;
+import com.sun.xml.wss.impl.policy.mls.MessagePolicy;
+import com.sun.xml.wss.impl.policy.mls.WSSPolicy;
+import java.util.List;
+
+/**
+ * A class which can select specific alternatives of a Username or a X509 appearing
+ * as SignedSupportingTokens.
+ */
+public class UsernameOrX509AlternativeSelector implements AlternativeSelector {
+
+
+
+	private enum SupportingTokenType {
+
+		USERNAME, X509, UNKNOWN
+	}
+
+	@Override
+	public MessagePolicy selectAlternative(ProcessingContext ctx, List<MessagePolicy> alternatives, SecurityPolicy recvdPolicy) {
+		SupportingTokenType reqMsgTokenType = determineTokenType(recvdPolicy);
+		for (MessagePolicy mp : alternatives) {
+			SupportingTokenType alternativeTokenType = determineTokenType(mp);
+			if (reqMsgTokenType != SupportingTokenType.UNKNOWN && reqMsgTokenType.equals(alternativeTokenType)) {
+				return mp;
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public boolean supportsAlternatives(List<MessagePolicy> alternatives) {
+		 if (alternatives.size() != 2) {
+			 return false;
+		 }
+		 SupportingTokenType firstAlternativeType = determineTokenType(alternatives.get(0));
+
+		 if(firstAlternativeType == SupportingTokenType.UNKNOWN) {
+			 return false;
+		 }
+
+		 SupportingTokenType secondAlternativeType = determineTokenType(alternatives.get(1));
+
+		 if(secondAlternativeType == SupportingTokenType.UNKNOWN) {
+			 return false;
+		 }
+
+		 if(firstAlternativeType == secondAlternativeType) {
+			 return false;
+		 }
+
+		 return true;
+	}
+
+	private SupportingTokenType determineTokenType(SecurityPolicy recvdPolicy) {
+		SupportingTokenType ret = SupportingTokenType.UNKNOWN;
+		if (recvdPolicy instanceof MessagePolicy) {
+			MessagePolicy pol = (MessagePolicy) recvdPolicy;
+			for (int i = 0; i < pol.size(); i++) {
+				try {
+					WSSPolicy p = (WSSPolicy) pol.get(i);
+					if (PolicyTypeUtil.usernameTokenBinding(p) || PolicyTypeUtil.usernameTokenBinding(p.getFeatureBinding())) {
+						ret = SupportingTokenType.USERNAME;
+						break;
+					} else if (PolicyTypeUtil.x509CertificateBinding(p) || PolicyTypeUtil.x509CertificateBinding(p.getFeatureBinding())) {
+						ret = SupportingTokenType.X509;
+						break;
+					}
+				} catch (Exception e) {
+					//nothing to do.
+				}
+			}
+		}
+		return ret;
+	}
+
+}
