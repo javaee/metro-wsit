e44fe3b0d379a5eb75748ce4f62c90acf3944bdd
Martin Grebac <martin.grebac@oracle.com>
2012-02-23 10:51:34 +0000

    replace with new apis
    svn path=/trunk/; revision=7026
    
    
    Former-commit-id: 9ee2eb1b6259c478599c001e702a1ae5f6d50fb0

--- wsit/soaptcp/soaptcp-impl/src/main/java/com/sun/xml/ws/transport/tcp/server/TCPServiceChannelWSAdapter.java
+++ wsit/soaptcp/soaptcp-impl/src/main/java/com/sun/xml/ws/transport/tcp/server/TCPServiceChannelWSAdapter.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2011 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -40,9 +40,9 @@
 
 package com.sun.xml.ws.transport.tcp.server;
 
+import org.jvnet.ws.message.DistributedPropertySet;
+import org.jvnet.ws.message.PropertySet;
 import com.sun.istack.NotNull;
-import com.sun.xml.ws.api.DistributedPropertySet;
-import com.sun.xml.ws.api.PropertySet;
 import com.sun.xml.ws.api.message.Packet;
 import com.sun.xml.ws.api.pipe.Codec;
 import com.sun.xml.ws.api.server.WSEndpoint;
