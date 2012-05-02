5fd48a6427e98534e5349fbb29e14c642664b1dd
Lukas Jungmann <lukas.jungmann@oracle.com>
2012-05-02 21:03:48 +0000

    WSIT-1619: java.lang.ClassCastException: org.glassfish.gmbal.ManagedObjectManagerNOPImpl cannot be cast to com.sun.xml.ws.server.WSEndpointMOMProxy
    svn path=/branches/2.2.0.1/; revision=7088
    
    
    Former-commit-id: e084f92d6c780c799ec328d4b5a3e68efc540df5

--- ws-rx/wsrm-impl/src/main/java/com/sun/xml/ws/rx/rm/runtime/sequence/SequenceManagerFactory.java
+++ ws-rx/wsrm-impl/src/main/java/com/sun/xml/ws/rx/rm/runtime/sequence/SequenceManagerFactory.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -124,7 +124,7 @@ public enum SequenceManagerFactory {
             return;
         }
 
-        if (!listener.canRegisterAtMOM() && !(managedObjectManager instanceof WSEndpointMOMProxy)) {
+        if (!listener.canRegisterAtMOM() && (managedObjectManager instanceof WSEndpointMOMProxy)) {
             // SequenceManager cannot be (un)registered directly so postpone its (un)registration until JMX connection
             // is created
             final WSEndpointMOMProxy endpointMOMProxy = (WSEndpointMOMProxy) managedObjectManager;
