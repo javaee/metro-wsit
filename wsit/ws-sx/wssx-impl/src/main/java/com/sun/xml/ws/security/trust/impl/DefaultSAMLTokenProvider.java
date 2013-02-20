ced80d89edb305ff4ba7537a311f372313f98c02
Martin Grebac <martin.grebac@oracle.com>
2013-02-20 12:35:34 +0000

    fix warning - before bom merge
    svn path=/trunk/; revision=7432
    
    
    Former-commit-id: da1cfe678c9659ed31fe8c2e083cab86fd0e5326

--- wsit/ws-sx/wssx-impl/src/main/java/com/sun/xml/ws/security/trust/impl/DefaultSAMLTokenProvider.java
+++ wsit/ws-sx/wssx-impl/src/main/java/com/sun/xml/ws/security/trust/impl/DefaultSAMLTokenProvider.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -179,6 +179,7 @@ public class DefaultSAMLTokenProvider implements STSTokenProvider {
         ctx.setUnAttachedSecurityTokenReference(samlReference);
     }
 
+    @SuppressWarnings("UnusedAssignment")
     public void isValideToken(IssuedTokenContext ctx) throws WSTrustException {
         WSTrustVersion wstVer = (WSTrustVersion)ctx.getOtherProperties().get(IssuedTokenContext.WS_TRUST_VERSION);
         WSTrustElementFactory eleFac = WSTrustElementFactory.newInstance(wstVer);
