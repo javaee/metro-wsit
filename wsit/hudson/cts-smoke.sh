a16df25f69c37c02e1ae7719f85a7b44864e186c
Lukas Jungmann <lukas.jungmann@oracle.com>
2013-05-28 13:14:23 +0000

    adopt changes in CTS bundle
    svn path=/trunk/; revision=7585
    
    
    Former-commit-id: ea720bcb4d15e8532f5646b44dd69549d7c5adaa

--- wsit/hudson/cts-smoke.sh
+++ wsit/hudson/cts-smoke.sh
@@ -2,7 +2,7 @@
 #
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
 #
-# Copyright (c) 2012 Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2012-2013 Oracle and/or its affiliates. All rights reserved.
 #
 # The contents of this file are subject to the terms of either the GNU
 # General Public License Version 2 only ("GPL") or the Common Development
@@ -177,9 +177,14 @@ if [ ! -z "$MAIL_HOST" ]; then
     sed -in 's/mailuser1=/mailuser1='"$MAIL_USER@$MAIL_HOST"'/g' ts.jte
     echo "CTS mailHost: $MAIL_HOST"
     sed -in 's/mailHost=/mailHost='"$MAIL_HOST"'/g' ts.jte
-    echo "CTS report.dir: $Y"
-    sed -in 's/report.dir=\/tmp\/JTreport/report.dir='"$Y"'/g' ts.jte
 fi
+
+echo "CTS report.dir: $Y"
+sed -in 's/report.dir=\/files\/JTReport/report.dir='"$Y"'/g' ts.jte
+
+echo "CTS work.dir: /tmp/JTWork"
+sed -in 's/work.dir=\/files\/JTWork/work.dir=\/tmp\/JTWork/g' ts.jte
+
 popd
 
 echo "Running GlassFish CTS smoke tests..."
