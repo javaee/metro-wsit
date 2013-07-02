5fb6890c37429fefca4093a904dbe46db8cd5877
Martin Grebac <martin.grebac@oracle.com>
2013-07-02 10:49:25 +0000

    fix failing tests
    svn path=/trunk/; revision=7620
    
    
    Former-commit-id: 711da28e126ba5613f77582090788976a8eaf6c0

--- wsit/ws-sx/wssx-impl/src/main/java/com/sun/xml/ws/security/opt/impl/message/SOAPBody.java
+++ wsit/ws-sx/wssx-impl/src/main/java/com/sun/xml/ws/security/opt/impl/message/SOAPBody.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -151,7 +151,7 @@ public class SOAPBody{
                     ((StAXEXC14nCanonicalizerImpl)writer).setInclusivePrefixList(prefixList);
                 }
             }
-            buffer.writeToXMLStreamWriter(writer);
+            buffer.writeToXMLStreamWriter(writer, true);
         }else{
             throw new UnsupportedOperationException();
             //TODO
