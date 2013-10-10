b2d361ad096f4d2972904287c2bb90c3a4c8141a
Lukas Jungmann <lukas.jungmann@oracle.com>
2013-10-10 19:57:49 +0000

    test clean up/update, added more tests, check for bundles' state
    svn path=/trunk/; revision=7744
    
    
    Former-commit-id: 886e87f1526b0d278d35b805379f694d46fdc611

--- wsit/tests/osgi-test/src/test/java/org/glassfish/metro/test/osgi/OSGITest.java
+++ wsit/tests/osgi-test/src/test/java/org/glassfish/metro/test/osgi/OSGITest.java
@@ -59,7 +59,7 @@ import org.osgi.framework.FrameworkUtil;
 /**
  * Set of simple tests which should guarantee that Metro imported/exported
  * packages can be properly loaded by GlassFish (or basically any OSGi framework).
- * 
+ *
  */
 @RunWith(PaxExam.class)
 @ExamReactorStrategy(PerClass.class)
@@ -70,24 +70,21 @@ public class OSGITest {
     private static final String JAXB_SPEC_VERSION = System.getProperty("jaxb.spec.version");
     private static final String JAXB_IMPL_VERSION = System.getProperty("jaxb.impl.version");
     private static final String METRO_VERSION = System.getProperty("metro.version");
-    private static final String HK2_VERSION = "2.1.64";
+    private static final String HK2_VERSION = System.getProperty("hk2.version", "2.2.0-b20");
     private static final String GF_VERSION = System.getProperty("glassfish.version");
+    private static final String PFL_VERSION = System.getProperty("pfl.version", "4.0.0-b003");
+
 
     @Configuration
     public static Option[] config() {
         return options(
-                repositories("http://repo1.maven.org/maven2",
-                //                "http://repository.apache.org/content/groups/snapshots-group",
-                //                "https://maven.java.net/content/groups/staging",
-                //                "http://repository.ops4j.org/maven2",
-                //                "http://svn.apache.org/repos/asf/servicemix/m2-repo",
-                //                "http://repository.springsource.com/maven/bundles/release",
-                //                "http://repository.springsource.com/maven/bundles/external",
-                                "http://maven.java.net/content/repositories/snapshots/"),
                 localRepository(System.getProperty("mvn.repo")),
+                repositories("http://repo1.maven.org/maven2",
+                             "https://maven.java.net/content/repositories/promoted/",
+                             "http://maven.java.net/content/repositories/snapshots/"),
                 mavenBundle().groupId("org.osgi").artifactId("org.osgi.compendium").version("4.3.0"),
+
                 systemPackage("com.sun.nio"),
-                systemPackage("sun.nio.cs"),
                 systemPackage("sun.rmi.rmic"),
                 systemPackage("sun.misc"),
                 systemPackage("com.ibm.security.util"),
@@ -95,55 +92,63 @@ public class OSGITest {
                 systemPackage("com.sun.net.httpserver"),
                 systemPackage("com.sun.tools.javac"),
                 systemPackage("sun.tools.javac"),
+                systemPackage("sun.corba"), // pfl
+
+                // APIs
+                mavenBundle().groupId("javax.annotation").artifactId("javax.annotation-api").version("1.2"),
+                mavenBundle().groupId("javax.enterprise.deploy").artifactId("javax.enterprise.deploy-api").version("1.6"),
+                mavenBundle().groupId("javax.interceptor").artifactId("javax.interceptor-api").version("1.2"),
+                mavenBundle().groupId("javax.mail").artifactId("javax.mail-api").version("1.5.0"),
+                mavenBundle().groupId("javax.resource").artifactId("javax.resource-api").version("1.7"),
+                mavenBundle().groupId("javax.servlet").artifactId("javax.servlet-api").version("3.1.0"),
+                mavenBundle().groupId("javax.transaction").artifactId("javax.transaction-api").version("1.2"),
+                mavenBundle().groupId("javax.xml.rpc").artifactId("javax.xml.rpc-api").version("1.1.1"),
+                mavenBundle().groupId("javax.xml.registry").artifactId("javax.xml.registry-api").version("1.0.5"),
+
                 // GlassFish
-                mavenBundle().groupId("org.glassfish.hk2").artifactId("config").version("2.0.5"),
-                mavenBundle().groupId("org.glassfish.hk2").artifactId("hk2").version(HK2_VERSION),
-                mavenBundle().groupId("org.glassfish.hk2").artifactId("hk2-locator").version(HK2_VERSION),
-                mavenBundle().groupId("org.glassfish.hk2").artifactId("hk2-core").version("2.0.5"),
-                mavenBundle().groupId("org.glassfish.hk2").artifactId("auto-depends").version("2.0.5"),
+                mavenBundle().groupId("org.glassfish.hk2").artifactId("class-model").version(HK2_VERSION),
                 mavenBundle().groupId("org.glassfish.hk2").artifactId("config-types").version(HK2_VERSION),
+                mavenBundle().groupId("org.glassfish.hk2").artifactId("core").version(HK2_VERSION),
+                mavenBundle().groupId("org.glassfish.hk2").artifactId("hk2").version(HK2_VERSION),
+                mavenBundle().groupId("org.glassfish.hk2").artifactId("hk2-api").version(HK2_VERSION),
+                mavenBundle().groupId("org.glassfish.hk2").artifactId("hk2-config").version(HK2_VERSION),
+                mavenBundle().groupId("org.glassfish.hk2").artifactId("hk2-locator").version(HK2_VERSION),
+                mavenBundle().groupId("org.glassfish.hk2").artifactId("hk2-runlevel").version(HK2_VERSION),
+                mavenBundle().groupId("org.glassfish.hk2").artifactId("hk2-utils").version(HK2_VERSION),
+                mavenBundle().groupId("org.glassfish.hk2").artifactId("osgi-resource-locator").version("1.0.1"),
+                mavenBundle().groupId("org.glassfish.hk2.external").artifactId("asm-all-repackaged").version(HK2_VERSION),
                 mavenBundle().groupId("org.glassfish.hk2.external").artifactId("bean-validator").version(HK2_VERSION),
                 mavenBundle().groupId("org.glassfish.hk2.external").artifactId("cglib").version(HK2_VERSION),
-                mavenBundle().groupId("org.glassfish.hk2.external").artifactId("asm-all-repackaged").version(HK2_VERSION),
-                mavenBundle().groupId("org.glassfish.hk2.external").artifactId("javax.inject").version("2.0.5"),
-                mavenBundle().groupId("org.glassfish.hk2").artifactId("hk2-api").version("2.0.5"),
-                mavenBundle().groupId("org.glassfish.hk2").artifactId("hk2-api").version(HK2_VERSION),
-                mavenBundle().groupId("org.glassfish.hk2").artifactId("hk2-runlevel").version(HK2_VERSION),
-                mavenBundle().groupId("org.glassfish.hk2").artifactId("hk2-config").version(HK2_VERSION),
-                mavenBundle().groupId("org.glassfish.hk2").artifactId("hk2-utils").version(HK2_VERSION),
-                mavenBundle().groupId("org.glassfish.hk2").artifactId("class-model").version(HK2_VERSION),
-                mavenBundle().groupId("org.glassfish.ha").artifactId("ha-api").version("3.1.9"),
+
                 mavenBundle().groupId("org.glassfish.external").artifactId("management-api").version("3.2.0-b001"),
-                mavenBundle().groupId("javax.annotation").artifactId("javax.annotation-api").version("1.2-b03"),
-//                mavenBundle().groupId("org.glassfish.main").artifactId("javax.enterprise.deploy").version(GF_VERSION),
-//                mavenBundle().groupId("org.glassfish.main").artifactId("javax.resource").version(GF_VERSION),
-//                mavenBundle().groupId("org.glassfish.main").artifactId("javax.transaction").version(GF_VERSION),
-                mavenBundle().groupId("org.eclipse.persistence").artifactId("javax.persistence").version("2.0.101.v201206242320"),
-                mavenBundle().groupId("javax.servlet").artifactId("javax.servlet-api").version("3.0.1"),
-                mavenBundle().groupId("org.glassfish.gmbal").artifactId("gmbal").version("3.1.0-b001"),
-                mavenBundle().groupId("org.glassfish.main.grizzly").artifactId("nucleus-grizzly-all").version(GF_VERSION),
-                mavenBundle().groupId("org.glassfish.main.common").artifactId("glassfish-api").version(GF_VERSION),
-                mavenBundle().groupId("org.glassfish.main.common").artifactId("annotation-framework").version(GF_VERSION),
-                mavenBundle().groupId("org.glassfish.hk2").artifactId("osgi-resource-locator").version("1.0.1"),
-                mavenBundle().groupId("org.glassfish.main.common").artifactId("internal-api").version(GF_VERSION),
-                mavenBundle().groupId("org.glassfish.hk2").artifactId("core").version(HK2_VERSION),
-                mavenBundle().groupId("org.glassfish.main.deployment").artifactId("deployment-common").version(GF_VERSION),
-                mavenBundle().groupId("org.glassfish.main.external").artifactId("ant").version(GF_VERSION),
-                mavenBundle().groupId("org.glassfish.main.common").artifactId("common-util").version(GF_VERSION),
+                mavenBundle().groupId("org.glassfish.gmbal").artifactId("gmbal").version("4.0.0-b001"),
+                mavenBundle().groupId("org.glassfish.ha").artifactId("ha-api").version("3.1.9"),
+
                 mavenBundle().groupId("org.glassfish.main.admin").artifactId("config-api").version(GF_VERSION),
-                mavenBundle().groupId("org.glassfish.main").artifactId("simple-glassfish-api").version(GF_VERSION),
-                mavenBundle().groupId("javax.transaction").artifactId("javax.transaction-api").version("1.2-b01"),
-                mavenBundle().groupId("javax.resource").artifactId("javax.resource-api").version("1.7-b03"),
-                mavenBundle().groupId("javax.enterprise.deploy").artifactId("javax.enterprise.deploy-api").version("1.6"),
+                mavenBundle().groupId("org.glassfish.main.common").artifactId("annotation-framework").version(GF_VERSION),
+                mavenBundle().groupId("org.glassfish.main.common").artifactId("common-util").version(GF_VERSION),
+                mavenBundle().groupId("org.glassfish.main.common").artifactId("glassfish-api").version(GF_VERSION),
+                mavenBundle().groupId("org.glassfish.main.common").artifactId("internal-api").version(GF_VERSION),
+                mavenBundle().groupId("org.glassfish.main.common").artifactId("simple-glassfish-api").version(GF_VERSION),
+                mavenBundle().groupId("org.glassfish.main.deployment").artifactId("deployment-common").version(GF_VERSION),
                 mavenBundle().groupId("org.glassfish.main.deployment").artifactId("dol").version(GF_VERSION),
-                mavenBundle().groupId("javax.xml.rpc").artifactId("javax.xml.rpc-api").version("1.1"),
-                mavenBundle().groupId("javax.xml.registry").artifactId("javax.xml.registry-api").version("1.0.4"),
-                mavenBundle("com.sun.mail", "javax.mail", "1.4.5"),
-                
-                
+                mavenBundle().groupId("org.glassfish.main.external").artifactId("ant").version(GF_VERSION),
+                mavenBundle().groupId("org.glassfish.main.grizzly").artifactId("nucleus-grizzly-all").version(GF_VERSION),
+
+                mavenBundle().groupId("org.glassfish.pfl").artifactId("pfl-asm").version(PFL_VERSION),
+                mavenBundle().groupId("org.glassfish.pfl").artifactId("pfl-basic").version(PFL_VERSION),
+                mavenBundle().groupId("org.glassfish.pfl").artifactId("pfl-dynamic").version(PFL_VERSION),
+                mavenBundle().groupId("org.glassfish.pfl").artifactId("pfl-tf").version(PFL_VERSION),
+
+                mavenBundle().groupId("org.eclipse.persistence").artifactId("javax.persistence").version("2.1.0"),
+                mavenBundle().groupId("org.jboss.weld").artifactId("weld-osgi-bundle").version("2.0.3.Final"),
+
+                mavenBundle().groupId("com.sun.mail").artifactId("javax.mail").version("1.5.0"),
+
                 //Metro APIs
-                mavenBundle("javax.xml.bind", "jaxb-api", JAXB_SPEC_VERSION),
-                mavenBundle("org.glassfish.metro", "webservices-api-osgi", METRO_VERSION),
+                mavenBundle("javax.xml.bind", "jaxb-api", JAXB_SPEC_VERSION).startLevel(1),
+                mavenBundle("org.glassfish.metro", "webservices-api-osgi", METRO_VERSION).startLevel(1),
+
                 //Metro OSGi bundles
                 mavenBundle("com.sun.xml.bind", "jaxb-osgi", JAXB_IMPL_VERSION),
                 mavenBundle("com.sun.xml.bind", "jaxb-extra-osgi", JAXB_IMPL_VERSION),
@@ -154,29 +159,51 @@ public class OSGITest {
     }
 
     @Test
+    public void testJaxbAPI() {
+        Class<?> c = loadClass("javax.xml.bind.JAXBContext");
+        assertClassLoadedByBundle(c, "jaxb-api");
+    }
+
+    @Test
+    public void testWsApiOSGi() {
+        Class<?> c = loadClass("javax.xml.ws.Service");
+        assertClassLoadedByBundle(c, "org.glassfish.metro.webservices-api-osgi");
+    }
+
+    @Test
+    public void testWsOSGi() {
+        Class<?> c = loadClass("com.sun.xml.ws.api.WSService");
+        assertClassLoadedByBundle(c, "org.glassfish.metro.webservices-osgi");
+    }
+
+    @Test
     public void testLocalization() {
         Assert.assertEquals("Missing WSDL_URI", WscompileMessages.WSIMPORT_MISSING_FILE());
     }
 
     @Test
     public void testXmlResolver() {
-        loadClass("com.sun.org.apache.xml.internal.resolver.CatalogManager");
+        Class<?> c = loadClass("com.sun.org.apache.xml.internal.resolver.CatalogManager");
+        //class is there but loaded from the system bundle
+        Assert.assertNull("xmlresolver not loaded from JDK", FrameworkUtil.getBundle(c));
     }
 
     @Test
     public void testContextFactory101() {
-        loadClass("com.sun.xml.bind.ContextFactory_1_0_1");
+        Class<?> c = loadClass("com.sun.xml.bind.ContextFactory_1_0_1");
+        assertClassLoadedByBundle(c, "com.sun.xml.bind.jaxb-osgi");
     }
 
     @Test
     public void testLogger() {
         Class<?> c = loadClass("com.sun.istack.logging.Logger");
-        assertClassLoadedByBundle(c, "com.sun.xml.bind");
+        assertClassLoadedByBundle(c, "com.sun.xml.bind.jaxb-osgi");
     }
 
     @Test
     public void testMsvDatatypes() {
-        loadClass("com.sun.msv.datatype.xsd.IntegerType");
+        Class<?> c = loadClass("com.sun.msv.datatype.xsd.IntegerType");
+        assertClassLoadedByBundle(c, "com.sun.xml.bind.jaxb-extra-osgi");
     }
 
     private Class<?> loadClass(String className) {
@@ -188,11 +215,12 @@ public class OSGITest {
         }
         return null;
     }
-    
+
     private void assertClassLoadedByBundle(Class<?> c, String bundle) {
         Bundle b = FrameworkUtil.getBundle(c);
         Assert.assertEquals("Class '" + c.getName() + "' was loaded by '"
                 + b.getSymbolicName() + "', expected was '" + bundle + "'",
                 bundle, b.getSymbolicName());
+        Assert.assertEquals("Bundle '" + bundle + "' is not running", Bundle.ACTIVE, b.getState());
     }
 }
