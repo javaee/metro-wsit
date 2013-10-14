c0d08f5633d81f23d3efa2c9658e7907aeb0c39b
Harold Carr <harold.carr@oracle.com>
2013-10-14 21:55:58 +0000

    updated a field to milliseconds to be in sync with metro rm (also added correct copyright headers)
    svn path=/trunk/; revision=7754
    
    
    Former-commit-id: 339b372af3e4be5e0f928cfd58b108758031a897

--- wsit/ws-rx/wsrm-api/src/main/java/com/oracle/webservices/oracle_internal_api/rm/ReliableMessagingFeature.java
+++ wsit/ws-rx/wsrm-api/src/main/java/com/oracle/webservices/oracle_internal_api/rm/ReliableMessagingFeature.java
@@ -1,5 +1,41 @@
 /*
- * Copyright (c) 2011, 2013, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2013 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
  */
 
 // GENERATED CODE.  DO NOT EDIT.
@@ -23,12 +59,12 @@ public class ReliableMessagingFeature
 
     /**
      * 
-       Specifies the period (in seconds) of a sequence manager maintenance task execution.
-       Default value: 60
+       Specifies the period (in milliseconds) of a sequence manager maintenance task execution.
+       Default value: 60000
      */
-    public int getSequenceManagerMaintenancePeriod() { return sequenceManagerMaintenancePeriod; }
-    private int sequenceManagerMaintenancePeriod = 60;
-    private void setSequenceManagerMaintenancePeriod(final int x) { sequenceManagerMaintenancePeriod = x; }
+    public long getSequenceManagerMaintenancePeriod() { return sequenceManagerMaintenancePeriod; }
+    private long sequenceManagerMaintenancePeriod = 60000;
+    private void setSequenceManagerMaintenancePeriod(final long x) { sequenceManagerMaintenancePeriod = x; }
 
     /**
      * 
@@ -71,7 +107,7 @@ public class ReliableMessagingFeature
         public ReliableMessagingFeature build() { return o; }
         // END MANUAL EDIT:
         public Builder enabled(final boolean x) { o.setEnabled(x); return this; }
-        public Builder sequenceManagerMaintenancePeriod(final int x) { o.setSequenceManagerMaintenancePeriod(x); return this; }
+        public Builder sequenceManagerMaintenancePeriod(final long x) { o.setSequenceManagerMaintenancePeriod(x); return this; }
         public Builder maxConcurrentSession(final int x) { o.setMaxConcurrentSession(x); return this; }
         public Builder userTransactionTimeout(final int x) { o.setUserTransactionTimeout(x); return this; }
     }
@@ -107,7 +143,7 @@ public class ReliableMessagingFeature
         int result = 3;
         result = 31 * result + getID().hashCode();
         result = 31 * result + (enabled ? 1 : 0);
-        result = 31 * result + (sequenceManagerMaintenancePeriod);
+        result = 31 * result + ((int) (sequenceManagerMaintenancePeriod ^ (sequenceManagerMaintenancePeriod >>> 32)));
         result = 31 * result + (maxConcurrentSession);
         result = 31 * result + (userTransactionTimeout);
         return result;
